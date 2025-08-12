b4b272a87fe5ba22642f568f4c6af5372bd92e37
Akshay Reddy <poddukuriakshayreddy1605@gmail.com>
2025-08-12 12:37:44 +0530

    remove others files in my branch

--- dupls.py
+++
@@ -1,6 +0,0 @@
-a=input("enter any string:")
-res=""
-for i in a:
-    if i not in res:
-        res += i
-print("string after removing duplicates:", res)        
--- pyramid.py
+++
@@ -1,3 +0,0 @@
-l = int(input("how many lines? : "))
-for i in range(l):
-    print(' ' * (l-i- 1) + '*' * (2 *i+ 1))
