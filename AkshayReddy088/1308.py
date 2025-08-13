c1afee4fc5f15e06947d911b8f2db103e2f1a719
Akshay Reddy <poddukuriakshayreddy1605@gmail.com>
2025-08-13 14:18:55 +0530

    vowels

---
+++ AkshayReddy088/1308.py
@@ -0,0 +1,22 @@
+'''
+Write  a  program  to  print  distinct  vowels  of  the  string  without  using  set
+
+1) Let  input  be   RaMA  rAo
+    What  is  the  output ?  ---> AO
+
+2) Hint  1:  Same  as   prog3e  with  minor  changes
+
+3) What  does  'hyd' . upper()  do ? --->  Returns  'HYD'
+'''
+
+string = input("Enter a string:")
+
+vowels = "AEIOU"
+output = ""
+
+for char in string.upper():
+    if char in vowels and char not in output:
+        output += char
+    
+print(output)
+
