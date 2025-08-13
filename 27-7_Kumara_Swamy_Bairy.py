105a603ed44131d92c57bce6b75b577c72e71fcf
Kumaraswamy96 <kumarbhairy176@gmail.com>
2025-08-13 22:36:47 +0530

    Add files via upload

---
+++ 27-7_Kumara_Swamy_Bairy.py
@@ -0,0 +1,47 @@
+x=eval(input('Enter 1st input:'))
+y=eval(input('enter 2nd input:'))
+z=eval(input('enter 3rd input:'))
+if x>y:
+     print(f'largest input:{x}')
+else:
+    print(f'largest input:{y}')
+if z>y:    
+    print(f'largest input:{z}')
+print()
+x=eval(input('Enter 1st input:'))
+y=eval(input('enter 2nd input:'))
+if x>y:
+     print(f'result:>')
+else:
+     (print(f'result:<')
+if x<y
+else
+     print(f'result:='))
+
+
+print()
+a=int(input('enter any +ve integer:'))
+if a%2==0:
+    print('a is even number')
+else:
+    print('a is odd number')
+
+print()
+a=int(input('enter any number:'))
+if a>0:
+    print('result 1')
+else:
+    (print('result -1')
+if a<0
+else    
+    print('result 0'))
+print()
+a=eval(input('enter length:'))
+b=eval(input('enter breadth:'))
+print(f' area=',a*b)
+print(f'perimeter=',2*(a+b))
+
+import math
+a=eval(input('radius of the sphere:'))
+volume= (4/3)*math.pi*math.pow(a,3)
+print(f'volume of the sphere:{volume}')
