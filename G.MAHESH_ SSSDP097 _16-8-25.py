032eac3069a7e0213af1578582fda8c926074522
gundarapumahesh <gundarapumahesh01@gmail.com>
2025-08-16 19:29:29 +0530

    Add files via upload

---
+++ G.MAHESH_ SSSDP097 _16-8-25.py
@@ -0,0 +1,199 @@
+# 1) What  are  the  outputs  if  input  is   [25 , 10.8 , 'Hyd' , True]   (Home  work)
+
+a = input('Enter  list  :  ')
+print(type(a)) # <class 'str'>
+print(a)	# string list: [25 , 10.8 , 'Hyd' , True]
+b = eval(a)# here string list is converted to list
+print(b)	# [25 , 10.8 , 'Hyd' , True]
+print(type(b))	# <class 'list'>
+
+
+
+
+# 2) Find  outputs (Home  work)
+
+a = [10, 20, 15, 18]
+b = a
+print(a  is  b) # True
+print(a  ==  b) # True
+b[2] = 12 # 12 is replace with 15 of index 2  of b
+print(a) # output: [10, 20, 12, 18]
+
+
+
+
+# 3) Find  outputs  (Home  work)
+
+a = [10 , 20 , 15 , 18]
+b = [100 , 200 , 150]
+print(a + b)# [10, 20, 15, 18, 100, 200, 150]
+#print(a + 5) # error as  we cannot concatenate list and int
+#print(a + '5')# error as  we cannot concatenate list and str
+#print([10 , 20] + (30 , 40)) # error as  we cannot concatenate list and tuple
+
+
+
+
+
+# 4) Tricky  program
+#  Find  outputs
+
+a = [1,2,3]
+b = [4,5,6]
+print(a , id(a))#[1,2,3] address of list object 'a'
+a += b 
+print(a , id(a))#[1,2,3,4,5,6] address of modified list object 'a'
+
+
+
+
+# 5) Tricky  program
+#  Find  outputs
+
+a = [1,2,3]
+b = [4,5,6]
+print(a , id(a))#[1,2,3] address of list object 'a'
+a  = a + b
+print(a , id(a))#[1,2,3,4,5,6] address of modified list object 'a'
+
+
+
+# 6) List  packing
+
+a = 25
+b = 10.8
+c = 'Hyd'
+d = True
+e = [a , b , c , d] 
+print(e) # output: [25,20.8,'Hyd',True]
+print(type(e))# <class 'list'>
+
+
+
+
+# 7) Find  outputs
+
+list = [25 , 10.8 , 'Hyd' ,  True]
+a , *b , c = list
+print('a : ' , a)# a:25
+print('b : ' , b)# b:[10.8 , 'Hyd']
+print('c : ' , c)# c:True
+print(type(b))	# <class 'list'>
+x , *y = list		
+print('x : ' , x)# x: 25
+print('y : ' , y)# y:[10.8,'Hyd',True]
+*p , q = list
+print('p : ' , p)# p:[25 , 10.8 , 'Hyd']
+print('q : ' , q)# q: True
+
+
+
+
+# 8) Find  outputs  (Home  work)
+
+list = [25 , 10.8 , 'Hyd' , True]
+#a , b , c , d , e = list	# error as excess elements in the list 
+a , b , *c , d , e = list
+print('a : ' , a)# a :  25
+print('b : ' , b)# b :  10.8
+print('c : ' , c)# c :  []
+print('d : ' , d)# d :  Hyd
+print('e : ' , e)# e :  True
+#a , b , *c , d , e , f = list	# error as excess elements in the list 
+
+
+
+
+# 9) Find  outputs  (Home  work)
+
+list = [25 , 10.8 , 'Hyd' , True]
+a , b , _  , d = list
+print('a : ' , a)# a: 25
+print('b : ' , b)# b: 10.8
+print('_ : ' , _)# _: Hyd
+print('d : ' , d)# d: True
+
+
+
+
+# 10) Find  outputs (Home  work)
+
+list = [25 , 10.8 , 'Hyd' , True , 3 + 4j]
+a , b , a , d , a = list
+print('a : ' , a)# a: 3+4j
+print('b : ' , b)# b: 10.8
+print('d : ' , d)# d: True
+
+
+
+
+# 11) Tricky  program
+# Find  outputs (Home  work)
+
+list = [25 , 10.8 , 'Hyd' , True , 3 + 4j]
+a , b ,  _ , d , _  = list
+print('a : ' , a)# a: 25
+print('b : ' , b)# b: 10.8
+print('_ : ' , _)# _: 3+4j
+print('d : ' , d)# d: True
+print('_: ' , _)# _: 3+4j
+
+
+
+
+# 12) Identify  error (Home  work)
+
+list = [25 , 10.8 , 'Hyd' , True , 3 + 4j]
+a , *b , c , *d , e  = list # error as multiple starred expressions not allowed
+
+
+
+
+# 13) Find  outputs  (Home  work)
+
+list = [[25 , 10.8] , 'Hyd' , True]   #  Nested  list
+a , b , c = list
+print('a :  ' , a)# a: [25 , 10.8]
+print('b :  ' , b)# b: Hyd
+print('c :  ' , c)# c: True
+
+
+
+
+# 14) Find  outputs  (Home  work)
+
+list = [[25 , 10.8] , 'Hyd' , True]
+[a , b] , c , d = list
+print('a : ' , a)# a: 25
+print('b : ' , b)# b: 10.8
+print('c : ' , c)# c: Hyd
+print('d : ' , d)# d: True
+#a , b , c , d = list # error as excess elements in the list 
+
+
+
+# 15) Comparing  Lists
+
+a = [10 , 20 , 15 , 18]
+b = [10 , 20 , 15 , 18]
+c = [10 , 20 , 25 , 9]
+d = [10 , 20 , 7 , 22]
+print(a == b)# True
+print(a is b)# False
+print(a < c)# True
+print(a > d)# True
+print(a >= c)# False
+print(a <= d)# False
+print(a != c)# True
+print(a != b)# False
+print(a == c)# False
+
+
+
+# 17) Comparing  Lists  (Home  work)
+
+a = [10 , 20 , 15 , 18]
+b = [20 , 18 , 15 , 10]
+print(a == b)# False
+print(a  is   b)# False
+
