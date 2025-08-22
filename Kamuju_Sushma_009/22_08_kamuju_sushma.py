82c3cf661cdacb237ac4fba166ffea24ce6701c1
unknown <sushmakamuju333@gmail.com>
2025-08-22 21:56:26 +0530

    22nd August Home Work

---
+++ Kamuju_Sushma_009/22_08_kamuju_sushma.py
@@ -0,0 +1,176 @@
+#Home Work-1
+#index()  and  count()  methods  demo  program   (Home  work)
+a = (10 , 20 , 15 , 12 , 14 , 15 , 18 , 19 , 15 , 12 , 25)
+#     0    1      2     3    4     5     6    7     8    9    10
+try:
+	i = a . index(15) #2
+	while  True:
+		print('15 is found at index : ' , i) 
+		i = a . index(15 , i + 1)
+except:
+		print(F'15  is  found  {a . count(15)}  times')
+#15 is found at index: 2
+#15 is found at index: 5
+#15 is found at index: 8
+#15 is found 3 times
+
+#Home Work-2
+#  How  to  modify  an  element  of  tuple ?    (Home  work)
+a  =  10 ,  20 ,  30 ,   40 ,  50
+#     0      1       2       3      4
+# a[2] = 35
+print(a)
+print(id(a))
+# How  to  modify  30  in  tuple  to  35
+i=a.index(30)
+a=a[:i]+(35,)+a[i+1:]
+print(a)
+print(id(a))
+
+#Home Work-3
+# How  to  delete  an  element  of  tuple ?   (Home  work)
+a  = 10 , 20 , 30 , 40 , 50
+#    0     1      2     3      4
+# a . remove(30) error, tuple is immutable
+# del  a[2] error, tuple is immutable
+# a . pop(2) error, tuple is immutable
+print(a) # (10 , 20 , 30 , 40 , 50)
+print(id(a)) #address of the tuple object, say 1000
+#How  to  remove  30  from  tuple  'a' 
+i=a.index(30)
+a=a[:i]+a[i+1:]
+print(a)
+print(id(a))
+
+#Home Work-4
+#  Nested   tuple  (Home  work)
+a = ( (10 , 20)  ,  (30 , 40 , 50)  ,  (60 , 70 , 80 , 90) )
+print(a) #( (10 , 20)  ,  (30 , 40 , 50)  ,  (60 , 70 , 80 , 90) )
+print(type(a)) #<class 'tuple'>
+print(len(a)) #3
+print(a[0])#How  to  print  1st  inner  tuple)
+print(a[1])#How  to  print  2nd  inner  tuple)
+print(a[2])#How  to  print  3rd  inner  tuple)
+print(a[0][1])#How  to  print  20)
+print(a[1][2])#How  to  print  50)
+print(a[2][3])#How  to  print  90)
+
+#Home Work-5
+# Find  outputs  (Home  work)
+a = ((10 , 20 , 30),)
+print(a[0])#How  to   print  inner  tuple)
+print(*a)#How  to   print  inner  tuple  in  another  way)
+print(a[0][0])#How   to  print   10)
+print(a[0][1])#How   to  print   20)
+print(a[0][2])#How   to  print   30)
+b = ((),)
+print(a[0])#How  to   print  inner  tuple  of  tuple  'b')
+print(*a)#How  to   print  inner  tuple  of  tuple  'b'  in  another  way)
+
+#Home Work-6
+#  Find  outputs (Home  work)
+a = ((10 , 20 , 30))
+print(a) #(10,20,30)
+print(*a) #10 20 30
+b = (())
+print(b) #()
+print(*b) # empty
+
+#Home Work-7
+# What  are  the  outputs  if  input  is  {10 , 20 , 15 , 18 , 20 , 12 , 18}
+a = input('Enter  Set  :  ')
+print(a) #'{10 , 20 , 15 , 18 , 20 , 12 , 18}'
+print(type(a)) #<class 'str'>
+b = eval(a) 
+print(b)#{10 , 20 , 15 , 18 , 20 , 12 , 18}
+print(type(b)) #<class 'set'>
+
+#Home Work-8
+#  Find  outputs  (Home  work)
+print({(10 , 20 , 30)}) #{10,20,30}
+# print({[10 , 20 , 30]}) set does not allow mutable elements
+# print({{10 , 20 , 30}}) set does not allow mutable elements
+# print({{}})set does not allow mutable elements
+
+#Home Work-9
+# How  to  print  set  in  differnet ways  (Home  work)
+a = {25 , True , 'Hyd' , 10.8}
+print('set  with  print  function: ', a) 
+print(a)
+print('Iterate  elements  of  set  with  for  loop')
+# How  to  iterate  set  with  for  loop
+for x in a:
+    print(x,end=" ")
+	
+#Home Work-10
+# Find  outputs  (Home  work)
+a = 'Hyd'
+b = True
+c = 25
+d = 1
+e = 'Hyd'
+s = {a , b , c , d , e}
+print(s) #{'Hyd', True, 25}
+print(len(s)) #3
+print(type(s)) #<class 'set'>
+
+#Home Work-11
+# Find  outputs  (Home  work)
+s = {'Hyd',  25,  True,  10.8 }
+print(s) #{'Hyd',  25,  True,  10.8 }
+a , b , c , d = s
+print(a) #'Hyd'
+print(b) #25
+print(c) #True
+print(d) #10.8
+
+#Home Work-12
+# Find  outputs  (Home  work)
+s = {'Hyd',  25,  True,  10.8 }
+print(s) #{'Hyd',  25,  True,  10.8 }
+a , *b = s
+print(a) #Hyd
+print(b) #[25,True,10.8]
+print(type(b)) #<class 'list'>
+
+#Home Work-13
+# Find  outputs  (Home  work)
+s = {'Hyd',  25,  True,  10.8 }
+print(s) # {'Hyd',  25,  True,  10.8 }
+a , *b , c = s
+print(a) #'Hyd'
+print(b) #[25,'True']
+print(c) #10.8
+
+#Home Work-14
+# Find  outputs  (Home  work)
+s = {20 , 10 , 20 , 10}
+print(s) #{20, 10}
+x , y = s 
+print(x) #20
+print(y) #10
+
+#Home Work-15
+# set()  function  demo  program  (Home  work)
+a = range(100 , 151 , 10)
+b = set(a)
+print(b) #{100,110,120,130,140,150}
+c = [10 , 20 , 15 , 18 , 10 , 50 , 20 , 12 , 18]
+d = set(c) 
+print(d) #{10,20,15,18,50,12}
+e = set('Rama  rAo')
+print(e) #{'R','a','m',' ','r','A','o'}
+# print(set(25))#error, argument should be only sequence
+print(set()) #{}
+
+'''
+set()  function
+-----------------
+1) What  does  set(sequence)  do ?  ---> Converts  sequence  to  set
+
+2) What  does  set(No-args)  do ?  --->  Returns  an  empty  set
+
+3) How  many  arguments  can  set()  function  take ?  ---> Zero  (or) One  but  not  more  than  one
+
+4) Is  set(non-sequence)  valid ?  --->  No  becoz  argument  should  be  sequence
+'''
