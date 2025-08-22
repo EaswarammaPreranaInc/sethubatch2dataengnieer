b32f43be843b9cc09bc87ac6c853f6bd9e0ab518
VARIKOLLA NAVEEN <147158048+naveen9550@users.noreply.github.com>
2025-08-22 14:47:47 +0530

    Add files via upload

---
+++ V Naveen_SSSSDP-II-104_19.08.2025.py
@@ -0,0 +1,202 @@
+'''
+1.Modify  the  following  program  with  walrus  operator
+
+Hint:  Call  index()  method  only  once
+'''
+a = [10 , 20 , 15 , 12 , 14 , 15 , 18 , 19 , 15 , 12 , 25]
+try:
+	i = -1
+	while  i := a . index(15 , i + 1):
+		print(i)
+except:
+	print(F'15  is  found  {a . count(15)}  times ')
+	
+
+
+
+
+#2. Write  a  program  to  determine  first  list  is  a  sublist  of  2nd  list  or  not.
+try:
+    first = eval(input("Enter the first list : ")) # [10,20,30]
+    second = eval(input("Enter the second list : ")) #[15 , 18 , 10 ,  12 ,  19 , 20 , 14 , 12 , 30 , 25 ,  16]
+    k = 0
+    for i in first:
+        if i in second[k:]:
+            k = second.index(i,k+1)
+    print("True")   
+except:
+    print('False')
+        
+
+
+
+
+
+#3. copy()  method  demo program  (Home  work)
+a = [10 , 20 , 15 , 18]
+b = a . copy()
+print(b) # [10 , 20 , 15 , 18]
+print(a  is  b) # False
+print(a  ==  b) # True
+c = a[:]
+print(c) # [10 , 20 , 15 , 18]
+print(a  is  c) # False
+print(a  ==  c) # True
+d = a
+print(d) # [10 , 20 , 15 , 18]
+print(a  is  d) # True
+print(a  ==  d) # True
+
+
+'''
+copy()  method
+------------------
+1) What  does  copy()  method  do ? --->  Returns  a  new  list  with  same  elements
+
+2) b = a . copy()
+    What  is  another  way  to  copy  list  elements  to  another list ?  --->  b = a[:]
+'''
+
+
+
+
+#4  Nested  List  demo  program  (Home  work)
+a = [[10 , 20 , 30 ,  40]  ,  [50 , 60 ,  70 , 80]  ,  [90 , 100 , 110 , 120] ]
+print(a) #[[10 , 20 , 30 ,  40]  ,  [50 , 60 ,  70 , 80]  ,  [90 , 100 , 110 , 120] ]
+print(len(a)) # 3
+print(a[0]) # How  to  print  1st  inner  list
+print(a[1]) # How  to  print  2nd  inner  list
+print(a[2]) # How  to  print  3rd  inner  list
+print(a[0][2]) # How  to  print  30
+print(a[1][3]) # How  to  print  80
+print(a[2][1]) # How  to  print  100
+
+
+'''
+What  is  a  nested  list ?  --->  A  list  in  another  list
+'''
+
+
+
+
+5#  Find  outputs  (Home  work)
+a=[ [10 , 20] , [30 , 40 , 50] , [60 , 70 , 80 , 90]]
+print(a[0]) # How  to  print  1st   inner  list
+print(a[1]) # How  to  print  2nd   inner  list
+print(a[2]) # How  to  print  3rd   inner  list
+print(len(a[0])) # How  to  print  number  of  elements  in  1st  inner  list
+print(len(a[1])) # How  to  print  number  of  elements  in  2nd  inner  list
+print(len(a[2])) # How  to  print  number  of  elements  in  3rd  inner  list
+
+
+
+#6  How  to  print  nested  list  in  differnent  ways
+a = [[10 , 20] , [30 , 40 ,  50] , [60 , 70 , 80 , 90]]
+print('Nested list  with  print function')
+print(a)
+print('Each  inner  list   of   outer  list  without  indexes')
+for i in a:
+    print(i) # How  to  print  each  inner  list  of  list  'a'  without  using  indexes  (use  for  loop)
+print('Elements  in  the  form   of  matrix   without  using  indexes')
+for i in a:
+    for j in i:
+        print(j, end= " ")
+    print() # How  to   print  elements  of  each  inner  list  without  using  indexes  in  matrix style  (use  nested  loop)
+print('Elements  in  the  form   of  matrix  using  indexes')
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        print(a[i][j], end= " ")
+    print() # How  to   print  elements  of  each  inner  list  using  indexes  in  matrix style (use  nested  loop)
+
+
+
+
+'''
+matrix   style
+----------------
+10    20
+30    40   50
+60    70   80   90
+'''
+
+
+
+
+#7  Find  outputs (Home  work)
+a = [[10 , 20] , [30 , 40] , [50 , 60] , [70 , 80]]
+for  x  in  a:
+    print(x) # [10 , 20] <nextline> [30 , 40] <nextline> [50 , 60] <nextline> [70 , 80]
+print()
+for  x , y  in  a:
+	print(x , y , sep = '...') # 10...20<nextline>30...40<nextline>50...60<nextline>70...80
+     
+
+
+
+#8  Find  outputs (Home  work)
+a = [[10 , 20 , 30] , [40 , 50 , 60] , [70 , 80 , 90]]
+for  x  in  a:
+    print(x) # [10, 20, 30]<nextline>[40, 50, 60]<nextline>[70, 80, 90]
+print()
+for  x , y ,  z  in   a:
+	print(x , y , z , sep = '...') # 10...20...30<nextline>40...50...60<nextline>70...80...90
+     
+
+
+
+
+#9  Find  outputs (Home  work)
+a = [[10 , 20] , [30 , 40 , 50] , [60 , 70 , 80 , 90]]
+for  x  in  a:
+	print(x)# [10, 20]<nextline>[30, 40, 50]<nextline>[60, 70, 80, 90]
+#for  x , y  in  a:
+#	print(x , y ,	sep = '...') # Error due to to many values to unpack
+      
+
+
+
+#10.  Find  outputs  (Home  work)
+a = [[]]
+print(a[0]) # How  to  print  inner  list
+print(*a) # How  to  print  inner  list  in  another  way
+
+
+
+
+#11  Find  outputs  (Home  work)
+a = [[10 , 'Rama' , 1000.0] , [20 , 'Sita' , 2000.0] , [15 , 'Rajesh' , 3500.0] , [18 , 'Kiran' , 2800.0] , [5 , 'Amar'  ,5000.0] ]
+print(sorted(a)) # [[5, 'Amar', 5000.0], [10, 'Rama', 1000.0], [15, 'Rajesh', 3500.0], [18, 'Kiran', 2800.0], [20, 'Sita', 2000.0]]
+print(sorted(a , reverse = True)) # [[20, 'Sita', 2000.0], [18, 'Kiran', 2800.0], [15, 'Rajesh', 3500.0], [10, 'Rama', 1000.0], [5, 'Amar', 5000.0]]
+print(a) # [[10, 'Rama', 1000.0], [20, 'Sita', 2000.0], [15, 'Rajesh', 3500.0], [18, 'Kiran', 2800.0], [5, 'Amar', 5000.0]]
+
+
+
+
+
+'''
+12. Tricky  program
+Write  a  program  to  determine  mode
+
+1) What  is  mode ?  ---> The  element  which  is  repeated  maximum  number  of  times  in  the  list
+
+2) Let  input  be  [12 , 20 , 18 , 15 , 10 ,  15 , 10 ,  15 ,  20 , 18 , 15 , 10 , 20 , 15 , 10]
+    What  is  set(list) ?  ---> {12 , 20 , 18 , 15 , 10}
+    How  many  times  is  first  element  12  repeated  in  the  list  ?  --->  1
+    How  many  times  is  2nd  element  20  repeated  in  the  list  ?  --->  3
+    How  many  times  is  3rd  element  18  repeated  in  the  list  ?  --->  2
+    How  many  times  is  4th  element  15  repeated  in  the  list  ?  --->  5
+    How  many  times  is  last  element  10  repeated  in  the  list  ?  --->  4
+    What  is  the  mode  ?  --->	15  becoz  it  is  repeated  max  number  of  times  i.e.  5
+
+3) mode = 15
+    ctr = 5
+'''
+listt = eval(input("Enter a list : " ))
+c = 0
+mode = 0
+for i in set(listt):
+    count = listt.count(i)
+    if count > c:
+        c = count
+        mode = i
+print("Mode is : ", mode)
