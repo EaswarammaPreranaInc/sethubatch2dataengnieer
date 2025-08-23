2624f0d2d354598cff640e4bd8791d630ff51558
phaneendra kumar <phaneendrak933@gmail.com>
2025-08-23 03:58:07 -0400

    PHANEENDRA_KUMAR_15-JULY_Dt.py

---
+++ PHANEENDRA_KUMAR_15-JULY_Dt.py
@@ -0,0 +1,128 @@
+# float  object  demo  program (Home  work)
+a = 10.8 output 
+print(a) ----->:10.8
+print(type(a) ----><class 'float'>
+print(id(a))------>adress of  'a' (maybe 4059)
+b = 25.
+print(b) -----> 25
+print(type(b))-----><class 'float'>
+c = .689
+print(c)------->0.689
+d = 3.4E2
+print(d)------>3.4*(10**2)=340
+print(type(d))-----><class 'float'>
+e= 9.62e-2
+print(e)----->0.0962
+print(9.8.2)---->syntax error
+ 
+
+
+
+
+
+
+# complex object demo program
+a = 3 + 4j
+print(a)------->(3+4j)
+print(type(a))-----------> <class 'complex'>
+print(id(a))--------------> adress of value 'a' maybe (9099)
+print(a . real)------->3.0
+print(a . imag)------>4.0
+print(type(a . real)-----><class 'float'>
+print(type(a . imag)-----><class 'float'>
+
+
+
+
+# Find outputs (Home work)
+a = 6j
+print(a)------>6j
+print(type(a))------><class 'complex'>
+print(a.real)------->0.0
+print(a.imag)------->6.0
+print(5 + j6)------->syntax error becoz of the imaginary part must be after the number .
+print(3 + 4i)------->python uses j as imaginary number  unlike maths and engineering .
+print(4+j)----------> j is undefined
+print(4 + 1j)-------->(4+1j)
+print(4 + 0j)--------->(4+0j)
+
+
+
+
+
+
+# bool object demo program  (Home  work)
+a = True
+print(a)------->True
+print(type(a))-----><class 'bool'>
+print(id(a))------> memory address of True
+b = False
+print(b)------->False
+print(type(b))-----><class 'bool'>
+print(True + True)------->1+1=2
+print(True + False)----->1+0=1
+print(False + True)------>0+1=1
+print(False + False)------>0+0=0
+print(True + True + True)----->1+1+1=3
+print(25 + 10.8 + True)------->25+10.8+1=36.8
+print(True > False)---------->True
+print(True)---------->True
+print(False)--------->False
+print(true)---------->error becoz of staring should be capital T
+print(false)--------->error becoz of staring should be capital F
+
+
+
+
+
+
+
+
+# Find  outputs (Home  work)
+a = 0O6247
+print(a)------>0o6247 = (6×512(2**9)) + (2×64(2**6)) + (4×8(2**3)) + (7×1(2**0))
+                       = 3072 + 128 + 32 + 7 = 3239
+print(type(a))------> <class 'int'> 
+print(id(a))--------->adress of value 'a'  maybe(1025)
+b = 0o6247
+print(id(b))---------->adress of value 'b'  maybe(1025)
+print(b)--------------->3239
+c = 3239
+print(c)------------->3239
+print(id(c))---------->adress of value 'c'  maybe(1025)
+print(0o9248)--------->syntax error due to digit 9.
+
+
+
+
+
+
+
+
+# Find  outputs  (Home  work)
+a = 0XA7B9
+print(a)------>(10*(2**12)+7*(2**8)+11*(2**4)+9*1)
+        ------->40960 + 1792 + 176 + 9 = 42937
+
+print(type(a))------>  <class 'int'>
+b = 0xBEEF
+print(b)--------->(11*(2**12)+14*(2**8)+14*(2**4)+15*1)
+        --------->45056 + 3584 + 224 + 15 = 48879
+print(A7B9)---->error becoz of name error 
+print('A7B9')---->A7B9
+print(0XBEER)---->syntax error due to invalid hexadecimal literal
+print(0XHYD)-----> syntax error due to invalid hexadecimal literal
+print(0xA7G9B)---->syntax error due to invalid hexadecimal literal
+
+
+
+
+
+
+# Find outputs (Home  work)
+a = 9248
+print(a)------>9248
+print(type(a))-----> <class 'int'>
+
+
+
