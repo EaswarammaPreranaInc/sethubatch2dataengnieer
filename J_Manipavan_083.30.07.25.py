24c1b05ca4c4985e5f8f8b4650d0d977d6f361d7
joginipallypavan <joginipallypavan@gmail.com>
2025-08-27 18:18:39 +0530

    Update J_Manipavan_083.30.07.25.py

--- J_Manipavan_083.30.07.25.py
+++ J_Manipavan_083.30.07.25.py
@@ -1,1 +1,26 @@
 
+
+
+'''
+Write  a  program  to  print  full  pyramid
+	 *
+   ***
+  *****
+ *******
+*********
+Input  is  number  of  lines
+'''
+a=int(input("How  many  lines ?  : "))
+for i in range(a):
+    for j in range(a-i-1):
+        print(" ",end="")
+    for j in range(2*i+1):
+        print("*",end="")
+    print()
+
+
+# In[ ]:
+
+
+
+
