556a4c382b16ef533bead04e0a9af5e0f926bda5
ParigiGanesh <162025380+ParigiGanesh@users.noreply.github.com>
2025-08-27 13:10:27 +0530

    Ganesh_SSSSDP-087

---
+++ Ganesh_SSSSDP-087-08-08-2025.py
@@ -0,0 +1,14 @@
+str=input('enter str input: ')    # 10
+for i in range(len(str)):
+	if i%2==0:
+		print('string at even index: ',str[i])
+else:
+	print('string at odd index: ',str[i])
+
+ # outputs
+	string at even index:  R
+    	string at even index:  m
+     	string at even index:   <space>
+    	string at even index:  a
+    	string at odd index:  o
+	
