f0540b15d04d4a987e360c2c6bd41a1d0af7cda4
rajubathini <118303907+Rajufhh@users.noreply.github.com>
2025-08-28 09:44:53 +0530

    Add files via upload

---
+++ 26-08_Bathini_Raju_SSSSDP_103.py
@@ -0,0 +1,22 @@
+'''
+Write  a  program  to  print  distinct  vowels  of  the  string  by  using  set
+
+1) Let  input  be  RamA  Rao
+    What  is  the  output  ?  --->  AO  (case  is  ignored)
+
+2) Both  input  and  output  are  strings
+
+3) Hint:  Same  as  prog19  with  minor  changes
+
+Enter  mixed  case  string :  RamA raO
+Distinct  vowels :   OA '''
+
+n=input("Enter String Input: ").upper()
+vowels="AEIOU"
+res=""
+for i in set(n):
+    if i in vowels:
+        res+=i
+print(res)
+        
+
