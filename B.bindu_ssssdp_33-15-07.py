a5047e548c4bdd0687becad951d0336392687e76
bandibindu2000-gif <bandibindu2000@gmail.com>
2025-08-28 01:37:25 -0700

    Add files via upload

---
+++ B.bindu_ssssdp_33-15-07.py
@@ -0,0 +1,88 @@
+# float  object  demo  program (Home  work)
+a = 10.8
+print(a) #10.8
+print(type(a)) #<class float>
+print(id(a))  #address of the object may be 1000
+b = 25.
+print(b) #25.0
+print(type(b)) #<class 'float'>
+c = .689 
+print(c)#0.689
+d = 3.4E2
+print(d) #340.0
+print(type(d)) #<class 'float'>
+e = 9.62e-2
+print(e) # 0.0962
+#print(9.8.2)#error due to so many decimal points
+print() #nothing
+# complex object demo program
+a = 3 + 4j
+print(a) #3+4j
+print(type(a)) #<class 'complex>
+print(id(a)) #address of the object
+print(a . real)#3.0
+print(a . imag) #4.0
+print(type(a . real))#<class 'float'>
+#print(type(a . image))#error
+print() #nothing
+# Find outputs (Home work)
+a = 6j
+print(a) #6j
+print(type(a)) #<class 'complex'>
+print(a.real) #0.0
+print(a.imag) #6.0
+#print(5 + j6)#error due to imag after number
+#print(3 + 4i)#error  due to 'i' is inovled
+#print(4+j)#error due to number is not there
+print(4 + 1j) #4+1j
+print(4 + 0j) #4+0j
+print() #nothing
+# bool object demo program  (Home  work)
+a = True
+print(a) #True
+print(type(a))#<class 'bool'>
+print(id(a))#address ofthe object
+b = False
+print(b)#False
+print(type(b))#>class 'bool'>
+print(True + True) #(1+1)-->2
+print(True + False)#1
+print(False + True)#1
+print(False + False)#0
+print(True + True + True)#3
+print(25 + 10.8 + True)#36.8
+print(True > False)#True
+print(True) #True
+print(False)#Flase
+#print(true)#error due to 't'is lowercase
+#print(false)#error due to 'f'is lowercase
+print() #nothing
+# Find  outputs (Home  work)
+a = 0O6247
+print(a) #3239 object contain decimal equivalent
+print(type(a))#<class 'int'>
+print(id(a))# address of the object
+b = 0o6247 
+print(id(b))
+print(b)#3239
+c = 3239#ref 'c'points to same object 
+print(c) #3239
+print(id(c))#address of the object
+#print(0o9248)#error
+print()
+# Find  outputs  (Home  work)
+a = 0XA7B9# object contain decimal equivalent
+print(a)#42937
+print(type(a))#,class 'int'>
+b = 0xBEEF
+print(b)#48879
+#print(A7B9)#error
+print('A7B9')
+#print(0XBEER)#error due to 'R' in hexa decimal number
+#print(0XHYD)#error due to 'H'&'Y' are involed
+#print(0xA7G9B)#error due to 'G' in hexa decimal number
+print()
+# Find outputs (Home  work)
+a = 9248
+print(a) #9248
+print(type(a))#<class 'int'>
