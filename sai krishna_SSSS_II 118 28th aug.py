433fb58a5b6e0fa37c2b1304bf5774cb7983c7ee
sarvagoni <164485564+sarvagoni@users.noreply.github.com>
2025-08-28 14:08:50 +0530

    Add files via upload

---
+++ sai krishna_SSSS_II 118 28th aug.py
@@ -0,0 +1,212 @@
+1.# Find outputs (Home  work)
+a = [ ('R' , 'Red') , ('G' , 'Green') , ('B' , 'Blue')]
+b = {'Y' : 'Yellow', 'G' : 'Gray'}
+b . update(a)
+print(b) # {'Y': 'Yellow', 'G': 'Green', 'R': 'Red', 'B': 'Blue'}
+a.update.b                      #  Error 'list' object has no attribute 'update'
+
+
+
+2.#  Find  outputs  (Home  work)
+a = [ (10 , 20 , 30) , (40 , 50 , 60) , (70 , 80 , 90)]
+b = {}
+#b.update(a)            # dictionary update sequence element #0 has length 3; 2 is required
+print(b)                # {}
+c = [(10,) , (20,) , (30,)]
+#b . update(c)          # dictionary update sequence element #0 has length 1; 2 is required
+
+print(b)               # {}
+
+
+3.#  Find  outputs (Home  work)
+d = {x : x ** 3   for    x   in  range(5)}
+print(d)                          # {0: 0, 1: 1, 2: 8, 3: 27, 4: 64}
+print(type(d))                    # <class 'dict'>
+
+
+4.# Find outputs   (Home  work)
+d = { x :  2 * x    for    x   in   range(5) }
+print(d)                     # {0: 0, 1: 2, 2: 4, 3: 6, 4: 8}
+
+
+
+5.# Find outputs  (Home  work)
+a = {10 : 'Rama' , 15 : 'Sita' , 18 : 'Rajesh' , 17 : 'Kiran' , 12 : 'Rama Rao'}
+b = { k :  v  for   k , v  in   a .items()   if    k % 2 != 0}
+print(b)                     # {15: 'Sita', 17: 'Kiran'}
+c = {k : a[k]   for   k   in    a    if   a[k] . startswith('R')}
+print(c)                     # {10: 'Rama', 18: 'Rajesh', 12: 'Rama Rao'}
+
+
+6.# Find outputs  (Home  work)
+def   f1():
+	print('Hyd')
+	print('Sec')
+	print('Cyb')
+# End  of  the  function
+print('Begin')                     
+x = f1()                           
+print(x)                           
+print(type(x))                     
+print('End')
+
+#output
+Begin
+Hyd
+Sec
+Cyb
+None
+<class 'NoneType'>
+End
+
+
+7.# Find  outputs  (Home  work)
+def  f1():
+	return  10 , 20 , 30
+# End  of  the  function
+x = f1()   
+print(x)                      # (10, 20, 30)
+print(type(x))                # <class 'tuple'>
+a , b , c =  f1()
+print(a)                      # 10
+print(b)                      # 20
+print(c)                      # 30
+print('for  loop')            # for loop
+for  k   in   f1():               
+	print(k)                  # 10
+	                          # 20
+	                          # 30       
+
+
+
+8.# Find  outputs
+def    f1():
+        return  10
+        return  20
+        return  30
+ End  of  the  function
+print('Begin')                          # Begin
+x = f1()                                # 10
+print(x)                                # End
+print('End')
+#return   100                           # Error : 'return' outside function
+
+
+9.#  Find  outputs
+f1()                            # name 'f1' is not defined
+def   f1():
+        print('Hello')            # Hello
+print(f1())                       # None
+print(f1)                         # <function f1 at 0x7c3b6e004180>
+
+
+10.# Find  outputs  (Home  work)
+print('Hello')                                   # Hello
+def  f1():
+        print('f1  function')
+#End  of   function
+print('Hi')                                      # Hi
+print(f1())                                      # fi function
+print(f1)                                        # None
+                                                  #<function f1 at 0x7d7ad3810180>
+
+print('Bye')                                    # Bye
+
+
+11.#  Find  outputs
+def    f1():
+        print('Hyd')
+        print('Sec')
+        print('Cyb')
+#End  of  the  function
+print('Begin')                            # Begin
+print(type(f1))                          # <class 'function'>
+print(id(f1))                              # 138495710151040
+print('End')                             # End
+
+
+
+12.#  Find  outputs (Home  work)
+def   add(a , b):
+        return  a + b
+#End  of  the  function
+print(add(10 , 20))                          # 30
+print(add('Hyder' , 'abad'))                 # Hyderabad
+print(add(10.8 , 20.6))                      # 31.400000000000002
+print(add(True , False))                     # 1
+print(add(3 + 4j , 5 + 6j))                  # (8+10j)
+print(add(25 , 10.8))                        # 35.8
+print(add([25 , 10.8 , 'Hyd'] , [True , None , 3+4j , 'Sec']))    # [25, 10.8, 'Hyd', True, None, (3+4j), 'Sec']
+
+print(add(10 , '20'))                         # Error
+
+
+13.'''
+Write   a  function  to  test  a  number  is  prime  (or)  not.
+1) What  is  a  prime  number ?  ---> A  number  without  divisors  except  1  and  itself
+
+2) Let  input  be  25
+    What  is  the  range  of  divisors ? ---> i =   2 , 3 , 4 , 5 , 6 , .....  12
+
+3) Let  input   be  11
+    What  is   the  range  of  divisors ? ---> i =  2 , 3 , 4 , 5
+
+4) What  action  to  be  made  if  'i'  is  not  a  divisor  of  input  number ?  ---> Move  to  the  next  element  of  range  object
+
+5) What  action  to  be  made  if  'i'  is  a  divisor  of  input  number ?  --->  return   False
+
+6) What  action  to  be  made  if  there  are  no  divisiors  to  input  number  ? ---> return  True  outside  the  loop
+'''
+def   prime(n):
+	return  true   when  'n'  is  prime  number  and  False  otherwise
+'''
+1) prime(25)  --->
+    How  many  times  is  for  loop  executed ?  --->
+
+2) prime(11) --->
+    How  many  times  is  for  loop  executed ?  --->
+
+3) prime(2) --->
+    How  many  times  is  for  loop  executed ?  --->
+
+4) prime(49) --->
+    How  many  times  is  for  loop  executed ?  --->
+'''
+How  to  read  a  number
+if   input  is  invalid:
+	print('Invalid  input')
+elif  input  is  prime  number:
+	print('Prime  number')
+else:
+	print('Composite  number')
+
+#.def prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+try:
+    num = int(input("Enter a number: "))
+    if prime(num):
+        print("Prime number")
+    else:
+        print("Composite number")
+except ValueError:
+    print("Invalid input")
+
+
+
+14.# Find  outputs  (Home  work)
+def   disp(empno , ename , sal):
+        print(F'Emp  Number  :  {empno} \t  Emp Name  :  {ename} \t  Salary  :  {sal}')
+# End  of  the  function
+disp(25 , 'Rama  Rao' , 10000.0)                    # Emp  Number  :  25 	  Emp Name  :  Rama  Rao 	  Salary  :  10000.0
+disp('Sita' , 20000.0 , 35)                         # Emp  Number  :  Sita 	  Emp Name  :  20000.0 	  Salary  :  35
+                   
+
+
+
+
