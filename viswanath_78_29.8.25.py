ad1c8565153ba4ffe5fd4111659b40f503fb4566
cvviswanath <viswanathvenkataram@gmail.com>
2025-08-29 22:18:36 +0530

    viswanath_78_29.8.25.py

---
+++ viswanath_78_29.8.25.py
@@ -0,0 +1,277 @@
+# Modify  following  program  such  that  every  function  should  be  executed
+def  f1():
+	print('No-argument  function')
+def  f1(x):
+	print('Single  argument  function  : ' , x)
+def  f1(x , y):
+	print('Two  argument  function : ' , x , y)
+def  f1(x , y , z):
+	print('Three  argument  function : ' , x , y , z)
+def  f1():
+	print('No-argument  function')
+ans)f1()
+def  f1(x):
+	print('Single  argument  function  : ' , x)
+f1(10)
+def  f1(x,y):
+	print('Two  argument  function : ' , x , y)
+f1(10,20)
+def  f1(x,y,z):
+	print('Three  argument  function :',x,y,z)
+f1(10,20,30)
+
+q)Write  a  program  to  generate  all   prime  numbers  between  2  and  n   and
+also  print  how  many  prime  numbers  are  existing
+Hint:  Use  the  prime()  function  defined  in   prog3a(prime).py  but  do  not  rewrite
+What  are  the  outputs  if  input  is  10  ?  --->  Prime   numbers 2 3 5 7
+Number  of   prime  numbers : 4
+How  to  read  a  number
+How  to  print  all  prime  numbers  between  2  and  user  input
+print('Number  of  prime numbers  :  ' ,  ???)
+Ans) from prog3a import prime
+n = int(input('Enter the number : '))  
+ctr = 0
+for i in range(2, n+1):
+    if prime(i):
+        print(i)  
+        ctr += 1
+print('Number of prime numbers :', ctr) 
+
+def f1(a , b , c):
+    print(f'a : {a} \t b : {b} \t c : {c}')  
+# End of the function
+f1(a = 10 , b = 20 , c = 30)  # a : 10 	 b : 20 	 c : 30
+f1(25 , 10.8 , 'Hyd')  # a : 25 	 b : 10.8 	 c : Hyd
+f1(b = 40.7 , a = 50.2 , c = 60.5)  # a : 50.2 	 b : 40.7 	 c : 60.5
+f1(c = 'Hyd' , b = 'Sec' , a = 'Cyb')  # a : Cyb 	 b : Sec 	 c : Hyd
+f1(c = 3 + 4j , a = True , b = None)  # a : True 	 b : None 	 c : (3+4j)
+f1(25 , c = 10.8 , b = 'Hyd')  # a : 25 	 b : Hyd 	 c : 10.8
+f1(a = 100 , 200 , 300)  # Error: positional argument follows keyword argument
+f1(True , None , b = 'Hyd')  # a : True 	 b : None 	 c : Hyd
+f1(10 , 20 , x = 30)  # Error: got an unexpected keyword argument 'x'
+f1(10 , 20)  # Error: missing 1 required positional argument
+
+def disp(empno , ename , sal):
+    print(f'Emp Number : {empno:4} \t Emp Name : {ename:15} \t Salary : {sal}')  
+# End of the function
+disp(25 , 'Rama Rao' , 10000.0)  # Emp Number :   25 	 Emp Name : Rama Rao       	 Salary : 10000.0
+disp(ename = 'Sita' , sal = 20000.0 , empno = 35)  # Emp Number :   35 	 Emp Name : Sita           	 Salary : 20000.0
+x = 'Rama Rao'
+y = 30000.0
+z = 20
+disp(x , y , z)  # Emp Number : Rama Rao 	 Emp Name : 30000.0       	 Salary : 20
+
+#  Tricky  program
+def f1(a , b , c):
+    return a + b * c
+# end of the function
+print(f1(3 , 4 , 5))  # 23
+print(f1(*[6 , 7 , 8]))  # 62
+print(f1([6 , 7 , 8]))  # Error: missing 2 required positional arguments
+print(f1(*{1 : 2 , 3 : 4 , 5 : 6}))  # 11 (keys {1,3,5} unpacked → a=1, b=3, c=5 → 1+3*5=16, but order depends on set iteration → actual result may differ)
+print(f1({'c' : 2 , 'b' : 4 , 'a' : 6}))  # Error: unsupported operand type(s) for *: 'str' and 'str'
+print(f1({'c' : 2 , 'b' : 4 , 'a' : 6}))  # Error: unsupported operand type(s) for *: 'str' and 'str'
+print({{'c' : 2 , 'b' : 4 , 'a' : 6}})  # Error: unhashable type: 'dict'
+print(f1({'c' : 2 , 'a' : 4 , 'x' : 6}))  # Error: unsupported operand type(s) for *: 'dict' and 'dict'
+
+# Identify  Error (Home  work)
+a = [10 , 20 , 15 , 5 , 12]
+print(sorted(reverse = True , a)) # Error: keyword arguments must come after positional ones 
+print(sorted(a , rev = True)) # Error: 'rev' is not a valid keyword, correct is 'reverse'
+print(25 , 10.8 , 'Hyd' , separator = '\t') # Error: 'separator' is invalid keyword, correct is 'sep'
+print(25 , 10.8 , 'Hyd' , endofline = '\t') # Error: 'endofline' is invalid keyword, correct is 'end'
+print(25 ,  sep = '\t' , 10.8 , end = '\t' , 'Hyd') # Error: positional argument follows keyword argument
+
+
+def f1(* , a , b):
+    print(f'a  :  {a}  \t  b :  {b}')
+# End of the function
+f1(a = 10 , b = 20)  # a  :  10  	 b :  20
+f1(b = 30 , a = 40)  # a  :  40  	 b :  30
+f1(50 , 60)  # Error: positional arguments not allowed
+f1(70 , b = 80)  # Error: positional argument not allowed for 'a'
+f1(a = 15 , 25)  # Error: positional argument follows keyword argument
+
+def f1(a , * , b , c):
+    print(f'a : {a}\tb : {b}\tc : {c}')
+# End of function
+f1(10 , b = 20 , c = 30)      # a : 10	b : 20	c : 30
+f1(a = 40 , b = 50 , c = 60)  # a : 40	b : 50	c : 60
+f1(c = 100 , b = 90 , a = 80) # a : 80	b : 90	c : 100
+f1(70 , 80 , c = 90)          # Error: b is keyword-only, cannot be passed positionally
+f1(70 , 80 , 90)              # Error: b and c are keyword-only, positional values not allowed
+f1(c = 15 , b = 25 , 35)      # Error: positional argument follows keyword argument
+
+# Identify error (Home  work)
+def f1(a, b, *): 
+    pass # Error: parameter name expected after '*'
+
+def f1(a , b , /):
+    print(f'a : {a}\tb : {b}')
+# End of the function
+f1(10 , 20)              # a : 10    b : 20
+f1(a = 30 ,  b = 40)     # Error: a and b are positional-only, cannot use keyword
+f1(50 , b = 60)          # Error: b cannot be passed as keyword, both must be positional
+f1(a = 70 , 80)          # Error: positional argument follows keyword argument
+
+def  f1(a , b , / , c):
+        print(F'a  :  {a}  \t  b :  {b}  \t  c  :  {c} ')
+# End  of  function
+f1(10 , 20 , 30)  # a  :  10  	b :  20  	c  :  30 
+f1(40 , 50 , c = 60) # a  :  40  	b :  50  	c  :  60 
+f1(a = 70 , b = 80 , c = 90) # Error: a and b are positional-only, cannot be passed as keyword arguments
+f1(a = 100 , b = 110 , 120) # Error: positional argument follows keyword argument
+f1(a = 130 , 140 , c = 150)  # Error: a is positional-only, cannot be passed as keyword
+f1(160 , b = 170 , 180)  # Error: positional argument follows keyword argument
+f1(190 , b = 200 , c = 210)# Error: a is positional-only, cannot be passed as keyword
+
+def  f1(a , b , / , c , d , * , e  , f):
+        print(F'a  :  {a}  \t  b  :  {b}  \t  c  :  {c}  \t  d  :  {d}  \t  e  :  {e}  \t  f  :  {f}')
+# End of the function
+f1(10 , 20 , 30 , d = 40 , e = 50 , f = 60) # a  :  10	b :  20  c  :  30  	d  :  40 	e  :  50  	f  :  60
+f1(1 , b = 2 , c = 3 , d = 4 , e = 5 , f = 6)  # Error: b is positional-only, cannot be passed as keyword
+f1(1 , 2 , 3 , 4 , 5 , f = 6) # Error: e is keyword-only, cannot be passed as positional
+f1(10 , 20 , c = 30 , 40 , e = 50 , f = 60) # Error: positional argument follows keyword argument
+f1(10 , 20 , 30 , 40 , e = 50 , f = 60) # a  :  10  	b :  20  	c  :  30  	d  :  40 	e  :  50  	f  :  60
+
+def  f1(/ , a , b ,  c):
+pass # Error: "/" cannot be placed at the start of parameter list
+def   f2(a , b , c , *):
+pass # Error:  "*" cannot be left alone, it must be followed by varargs name
+
+def f4(* , a , b , c , /):
+        pass # Error: parameters cannot be defined as keyword-only (*) first and then positional-only (/)
+
+def f1(x):
+    print('1st function : ' , x)
+def f1(y):
+    print('2nd function : ' , y)
+def f1(z):
+    print('3rd function : ' , z)
+f1(z = 10)   # 3rd function : 10
+f1(y = 20)   # Error: got an unexpected keyword argument 'y'
+f1(x = 30)   # Error: got an unexpected keyword argument 'x'
+
+def add(a , b = 20 , c = 30):
+    return a + b + c
+#end of the function
+print(add(100))                 # 100 + 20 + 30 = 150
+print(add(100 , 200))           # 100 + 200 + 30 = 330
+print(add(100 , 200 , 300))     # 100 + 200 + 300 = 600
+print(add(100 , c = 200))       # 100 + 20 + 200 = 320
+print(add(c = 100 , b = 200 , a = 300))  # 300 + 200 + 100 = 600
+print(add(c = 100 , a = 200))   # 200 + 20 + 100 = 320
+print(add())                    # Error: missing required argument 'a'
+print(add(a = 100 , 200))       # Error: positional argument follows keyword argument
+print(add(100 ,  , 300))        # Error: invalid syntax (double comma not allowed)
+print(add(100 ,  b , 300))      # Error: name 'b' is not defined
+
+ identify the error
+def f1(a = 10 , b , c = 20 , d):   # Error: non-default argument 'b' follows default argument
+    pass                           # Error: non-default argument 'd' follows default argument
+def f2(b , d , a = 10 , c = 20):   # Correct
+    pass
+def f1(a = 10):
+        print(a)
+# End of the function
+f1(20)      # 20
+f1()        # 10
+f1(a = 30)  # 30
+
+def add(a , b , c = 10 , d = 20):
+        return a + b + c + d
+# End of the function
+print(add(100 , 200))                # 100 + 200 + 10 + 20 = 330
+print(add(100 , 200 , 300))          # 100 + 200 + 300 + 20 = 620
+print(add(100 , 200 , 300 , 400))    # 100 + 200 + 300 + 400 = 1000
+print(add(b = 100 , a = 200))        # 200 + 100 + 10 + 20 = 330
+print(add(100 , 200 , d = 300))      # 100 + 200 + 10 + 300 = 610
+print(add(d = 100 , a = 200 , b = 300)) # 200 + 300 + 10 + 100 = 610
+print(add(c = 100 , d = 200 , 300 , 400)) # Error: positional argument follows keyword argument
+print(add(100 , 200 , c = 300 , x = 400)) # Error: got an unexpected keyword argument 'x'
+print(add())  # Error: missing required arguments 'a' and 'b'
+
+def f1(x = 25):
+    return x
+def f2(x):
+    return x
+# End of the function
+print(f1(10))  # 10
+print(f1())    # 25
+print(f2(20))  # 20
+print(f2())    # Error: missing required argument
+
+def disp(ch = '*' , n = 4):
+    print(ch * n)
+disp('-' , 6)       # ------
+disp('$')           # $$$$
+disp()              # ****
+disp(n = 5)         # *****
+disp(5)             # 5555
+disp(n = 7 , ch = '%')  # %%%%%%%
+disp(7 , '@')       # @@@@@@@
+disp(7 , n = 6)     # Error: multiple values for argument 'n'
+disp(ch = '!' , 5)  # Error: positional argument follows keyword argument
+
+def  power(a , b  =  2):
+        return  a ** b
+#end of the function
+print(power(2 , 6)) # 64
+print(power(5)) # 25
+print(power(b = 3 , a = 4.5)) # 91.125
+print(power(3 + 4j)) # (-7+24j)
+print(power(True)) # 1
+def   power(b = 2 , a):
+ 	 pass # SyntaxError: non-default argument follows default argument
+
+# Find outputs  (Home  work)
+def   add(a , b):
+	print('2-argument  function')
+	return a + b
+def  add(a , b , c):
+	print('3-argument  function')
+	return a + b + c
+def  add(a  = 1 , b  = 2 , c   = 3 , d = 4):
+	print('4-argument  function')
+	return a + b  + c + d
+# end  of  the  function
+# last function will be called
+print(add(10 , 20 , 30 , 40))  #  4-argument  function 100
+print(add(50 , 60 , 70)) # 4-argument  function 184
+print(add(80 , 90)) # 4-argument  function 177
+print(add(100)) # 4-argument  function 109
+print(add()) # 4-argument  function 10
+
+def  disp(a , b):
+        print('2-argument function  :  ' , a , b)
+def  disp(a , b , c , d):
+        print('4-argument  function  :  ' , a , b , c , d)
+def disp(a , b , c = 25):
+        print('3-argument  function  :  ' , a , b , c)
+#end of  the  function
+disp(10 , 20 , 30)  # 3-argument  function  :  10 20 30
+disp(40 , 50 , 60 , 70) # TypeError: disp() takes from 2 to 3 positional         arguments but 4 were given
+disp(80 , 90) # 3-argument  function  :  80 90 25
+
+def   add(* , a = 10 , b = 20):
+        return  a + b
+# End of  the  function
+print(add(a = 30 , b = 40)) # 70
+print(add()) # 30
+print(add(a = 50)) #70
+print(add(b = 60 , a = 70)) # 130
+print(add(80 , 90)) # TypeError: add() takes 0 positional arguments but 2 were given
+
+# Find  outputs(Home  work)
+def   add(a = 10 , b , c):
+        pass
+def   add( * , a = 10 , b , c ):
+        return  a + b + c
+# End  of  the  function
+print(add(a = 30 , b = 40 , c = 50)) # 120
+print(add(b = 60 , c = 70)) # 140
+print(add(c = 80 , b = 90 , a = 100)) # 270
+print(add(c = 25 , a = 43)) # TypeError: add() missing 1 required keyword-only argument: 'b'
+print(add(1 , 2 , 3)) # TypeError: add() takes 0 positional arguments but 3 were given
+def   add(a , b = 10 ,  c ,  * , d  , e = 20 , f):
+		pass # SyntaxError: non-default argument follows default argument
