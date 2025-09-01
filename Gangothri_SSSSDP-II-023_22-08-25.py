6b17d9c39b58e75d8b0b8dd8e167536d3f50cf42
Gangothri-16 <gangothripettam@gmail.com>
2025-09-01 08:47:48 +0530

    Add files via upload

---
+++ Gangothri_SSSSDP-II-023_22-08-25.py
@@ -0,0 +1,160 @@
+#index()  and  count()  methods  demo  program   (Home  work)
+a = (10 , 20 , 15 , 12 , 14 , 15 , 18 , 19 , 15 , 12 , 25)
+#     0    1      2     3    4     5     6    7     8    9    10
+try:
+	i = a . index(15)
+	while  True:
+		print('15 is found at index : ' , i)
+		i = a . index(15 , i + 1)
+except:
+		print(F'15  is  found  {a . count(15)} times')
+'''Output:
+15 is found at index: 2
+15 is found at index: 5
+15 is found at index: 8
+15 is found 3 times
+'''
+#  How  to  modify  an  element  of  tuple ?    (Home  work)
+a  =  10 ,  20 ,  30 ,   40 ,  50
+#     0      1       2       3      4
+a[2] = 35 # Error because tuple is immutable
+print(a) # (10,20,30,40,50)
+print(id(a)) # address of object a 
+print(tuple(list(a))) # (10,20,30,40,50)
+print(a[:2] + (35,)+a[3:])#How  to  modify  30  in  tuple  to  35
+print(a) # (10,20,35,40,50)
+print(id(a)) # address of updated tuple
+
+# How  to  delete  an  element  of  tuple ?   (Home  work)
+a  = 10 , 20 , 30 , 40 , 50
+#    0     1      2     3      4
+a.remove(30) # Error because tuple does not have remove method
+del  a[2] # not possible
+a.pop(2) # Not possible
+print(a) #(10,20,30,40,50)
+print(id(a)) # Address of object a
+print(a[:2]+a[3:]) #How  to  remove  30  from  tuple  'a'
+print(a) # (10,20,40,50)
+print(id(a)) # address  of (10,20,40,50)
+
+#  Nested   tuple  (Home  work)
+a = ( (10 , 20)  ,  (30 , 40 , 50)  ,  (60 , 70 , 80 , 90) )
+print(a) # ( (10 , 20)  , (30 , 40 , 50)  , (60 , 70 , 80 , 90) )
+print(type(a)) # <class 'tuple'>
+print(len(a)) # 3
+print('How  to  print  1st  inner  tuple')
+print(a[0])
+print('How  to  print  2nd  inner  tuple')
+print(a[1])
+print('How  to  print  3rd  inner  tuple')
+print(a[2])
+print('How  to  print  20')
+print(a[0][1])
+print('How  to  print  50')
+print(a[1][2])
+print('How to print 90')
+print(a[2][3])
+
+# Find  outputs  (Home  work)
+a = ((10 , 20 , 30),)
+print('How  to   print  inner  tuple')
+print(a[0])
+print('How  to   print  inner  tuple  in  another  way')
+print(*a)
+print('How   to  print   10')
+print(a[0][0])
+print('How   to  print   20')
+print(a[0][1])
+print('How   to  print   30')
+print(a[0][2])
+b = ((),)
+print('How  to   print  inner  tuple  of  tuple  'b'')
+print(b[0])
+print('How  to   print  inner  tuple  of  tuple  'b'  in another way')
+print(*b)
+
+#  Find  outputs (Home  work)
+a = ((10 , 20 , 30))
+print(a) # ((10, 20, 30))
+print(*a) # 10 20 30
+b = (())
+print(b) # ()
+print(*b) # Nothing is printed
+
+# What  are  the  outputs  if  input  is  {10 , 20 , 15 , 18 , 20 , 12 , 18}
+a = input('Enter  Set  :  ')
+print(a) # {10 , 20 , 15 , 18 , 20 , 12 , 18}
+print(type(a)) # <class 'str'>
+b = eval(a) # set 
+print(b) # same elements but not in order any order and no duplicates {18, 20, 10, 12, 15}
+print(type(b)) # <class 'set'>
+
+#  Find  outputs  (Home  work)
+print({(10 , 20 , 30)}) # {(10, 20, 30)}
+print({[10 , 20 , 30]}) # Error
+print({{10 , 20 , 30}}) # Error
+print({{}}) # Error
+
+# How  to  print  set  in  differnet ways  (Home  work)
+# How  to  print  set  in  differnet ways  (Home  work)
+a = {25 , True , 'Hyd' , 10.8}
+print(set(a)) #print('set  with  print  function')
+print(a)
+#print('Iterate  elements  of  set  with  for  loop')
+for x in a:
+    print(x)
+
+# Find  outputs  (Home  work)
+a = 'Hyd'
+b = True
+c = 25
+d = 1
+e = 'Hyd'
+s = {a , b , c , d , e}
+print(s) # set without duplicates in any order
+print(len(s)) # 3
+print(type(s)) # <class 'set'>
+
+# Find  outputs  (Home  work)
+s = {'Hyd',  25,  True,  10.8 }
+print(s) # {'Hyd',  25,  True,  10.8 } but any order not exactly this order
+a , b , c , d = s
+print(a) # 'Hyd'
+print(b) # 25
+print(c) # True
+print(d) # 10.8
+
+# Find  outputs  (Home  work)
+s = {'Hyd',  25,  True,  10.8 }
+print(s) # {25, 10.8, True, 'Hyd'}
+a , *b = s
+print(a) # 25
+print(b) [10.8, True, 'Hyd']
+print(type(b)) # <class 'list'>
+
+# Find  outputs  (Home  work)
+s = {'Hyd',  25,  True,  10.8 }
+print(s) # {25, 10.8, 'Hyd', True}
+a , *b , c = s
+print(a) # 25
+print(b) # [10.8, 'Hyd']
+print(c) # True
+
+# Find  outputs  (Home  work)
+s = {20 , 10 , 20 , 10}
+print(s) # {10, 20}
+x , y = s
+print(x) # 10
+print(y) # 20
+
+# set()  function  demo  program  (Home  work)
+a = range(100 , 151 , 10)
+b = set(a)
+print(b) # {130, 100, 140, 110, 150, 120}
+c = [10 , 20 , 15 , 18 , 10 , 50 , 20 , 12 , 18]
+d = set(c)
+print(d) # {10, 12, 15, 18, 50, 20}
+e = set('Rama  rAo')
+print(e) # {'R', ' ', 'o', 'a', 'A', 'm', 'r'}
+print(set(25)) # error because non sequence
+print(set()) # set()
