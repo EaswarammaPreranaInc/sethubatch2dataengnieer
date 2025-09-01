7fcdb5c90da37e061cca17cf298eddf3360e0daa
Yatam Praveen <praveen.yatam2001@gmail.com>
2025-09-01 22:35:01 +0530

    YatamPraveen_096_22-07-2025 .py

--- YatamPraveen_096_22-07-2025 .py
+++ YatamPraveen_096_22-07-2025 .py
@@ -1,8 +1,8 @@
 #  Find  outputs
-print({10 , 20}  |  {30 , 20})               #{10, 20, 30}
+print({10 , 20}  |  {30 , 20})                  #{10, 20, 30}
 print({10 : 'Hyd' , 20 : 'Sec'} |  {30 : 'Cyb' , 20 : 'Vja'})         #{10 : 'Hyd' , 20 : 'Vja' , 30 : 'Cyb'}
-print(range(4) | range(5))                #Returns error as range objects can't be concatenated
-print([10 , 20]  |  [30 , 20])                #Returns error as list concatenate by + not by |
+print(range(4) | range(5))                      #Returns error as range objects can't be concatenated
+print([10 , 20]  |  [30 , 20])                  #Returns error as list concatenate by + not by |
 
 
 
@@ -10,16 +10,16 @@ print([10 , 20]  |  [30 , 20])                #Returns error as list concatenate
 
 #  Assignment  operators  demo  program  (Home  work)
 a = 25
-print(a)                      #25
+print(a)                   #25
 b =  a
-print(b)                      #25
-print(a  is  b)              #True
+print(b)                   #25
+print(a  is  b)            #True
 x = 4
 y = 5
 z = x + y * 6
-print(z)                      #34
-25 = a                       #Returns error as it is supposed to be 25 = a not the other way around
-a + b = x + y            #Returns error as the left hand operand of = operator must be a reference variable not an object or expression
+print(z)                   #34
+25 = a                     #Returns error as it is supposed to be 25 = a not the other way around
+a + b = x + y              #Returns error as the left hand operand of = operator must be a reference variable not an object or expression
 
 
 
@@ -38,9 +38,9 @@ print(a , b , c)        #25 25 25
 
 # Multiple  Assignment (Home work)
 x , y , z = 25 , 10.8 , 'Hyd'
-print(x)                   #25
-print(y)                   #10.8
-print(z)                   #Hyd
+print(x)                         #25
+print(y)                         #10.8
+print(z)                         #Hyd
 
 
 
@@ -59,10 +59,10 @@ print(a)                    #27 because this is interpreted as a=a*(b+c)
 # Find outputs (Home work)
 a = 20
 a %= 3 + 2 * 4
-print(a)                 #9
+print(a)                       #9
 a = 3
 a **= 4
-print(a)              #81
+print(a)                       #81
 
 
 
@@ -72,8 +72,8 @@ print(a)              #81
 a = 25
 b = 25
 print(a  is  b)                     #True
-print(a  is  not  b)              #False
-print(a == b)                     #True
+print(a  is  not  b)                #False
+print(a == b)                       #True
 
 
 
@@ -83,8 +83,8 @@ print(a == b)                     #True
 a = 25
 b = 25.0
 print(a  is  b)                   #False
-print(a  is  not  b)            #True
-print(a == b)                   #True
+print(a  is  not  b)              #True
+print(a == b)                     #True
 
 
 
@@ -93,22 +93,22 @@ print(a == b)                   #True
 # Find outputs (Home work)
 a = 'Hyd'
 b = 'Hyd'
-print(a  is  b)                       #True
-print(a  is  not  b)                #False
-print(a == b)                       #True
-print()                                  #No output
+print(a  is  b)                         #True
+print(a  is  not  b)                    #False
+print(a == b)                           #True
+print()                                 #Blank line
 x = [1 , 2 , 3 , 4]
 y = [1 , 2 , 3 , 4]
 print(x is y)                          #False
-print(x  is  not  y)                 #True
-print(x == y)                        #True
-print()                                  #No output
+print(x  is  not  y)                   #True
+print(x == y)                          #True
+print()                                #Blank line
 m = (1 , 2 , 3 , 4)
 n = (1 , 2 , 3 , 4)
 print(m  is  n)                       #True
-print(m  is  not  n)                #False
-print(m == n)                       #True
-print(x == m)                       #False
+print(m  is  not  n)                  #False
+print(m == n)                         #True
+print(x == m)                         #False
 
 
 
@@ -118,17 +118,17 @@ print(x == m)                       #False
 list = [10 , 20 , 15 , 12 , 18]
 print(15 in list)                        #True
 print(19 in list)                        #False
-print(14 not in list)                 #True
-print(15 not in list)                 #False
+print(14 not in list)                    #True
+print(15 not in list)                    #False
 s = 'Hyd is green city'
-print( 'is' in s)                         #True
-print('was' in s)                      #False
+print( 'is' in s)                        #True
+print('was' in s)                        #False
 print('g' in s)                          #True
-print('z' in s)                           #False
-print(' ' in s)                            #True
-print('gre' in s)                       #True
+print('z' in s)                          #False
+print(' ' in s)                          #True
+print('gre' in s)                        #True
 print('yd i' in s)                       #True
-print('' in s)                             #True
+print('' in s)                           #True
 print('' not in s)                       #False
 
 
@@ -144,10 +144,10 @@ b = (4 , 2 , 3 , 1)
 print(a == b)                        #False
 p = {1 , 2 , 3 , 4}
 q = {4 , 1 , 3 , 2}
-print(p == q)                       #True
+print(p == q)                        #True
 m = range(5)
 n = range(5)
-print(m == n)                      #True
+print(m == n)                        #True
 
 
 
@@ -157,4 +157,5 @@ print(m == n)                      #True
 a = [10,20,30]
 b = (10,20,30)
 print(a  is  b)                   #False
-print(a  ==  b)                #False
+
+print(a  ==  b)                   #False
