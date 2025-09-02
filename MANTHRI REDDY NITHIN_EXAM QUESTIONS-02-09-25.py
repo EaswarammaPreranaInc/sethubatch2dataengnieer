1e2d77b327b16e7c69ad3c283ed50b5a216ba12b
REDDY80-dev <reddy23062002@gmail.com>
2025-09-02 22:49:34 +0530

    MANTHRI REDDY NITHIN_EXAM QUESTIONS-02-09-25.py

---
+++ MANTHRI REDDY NITHIN_EXAM QUESTIONS-02-09-25.py
@@ -0,0 +1,147 @@
+#Exam Questions
+'''
+Write  a  program  to  evaluate  expression  like  calculator
+Let  input  be  3 + 4 * 5 - 6 / 2 =
+What  is  the  output ? --->  14.5
+Hint:  Use  while  loop
+Iteration         a          op        b        result
+------------------------------------------------------
+       1               3          +          4           7   --->  'a'
+	   2              7          *          5           35   --->  'a'
+	   3             35         -          6           29   --->  'a'
+	   4             29         /          2           14.5   --->  'a'
+	   5            14.5       =          ---           ----
+'''
+#Program
+exp=input("Enter an Expresion: ")
+a=int(exp[0])
+i=1
+while exp[i]!='=':
+    if exp[i]=='+':
+        a=a+int(exp[i+1])
+    elif exp[i]=='*':
+        a=a*int(exp[i+1])
+    elif exp[i]=='-':
+        a=a-int(exp[i+1])
+    elif exp[i]=='/':
+        a=a/int(exp[i+1])
+    i+=2
+print(a)
+
+'''
+Write  a  program  to  print  each  digit  of  the  number  in  words
+Let  input  be  9247
+What  is  the  output  ?  ---> Nine  Two  Four  Seven
+a = ['Zero' , 'One' , 'Two' ,....  'Nine']
+Iteration     ch     int(ch)       a[int(ch)]         s
+--------------------------------------------------------
+                                                                     ''
+     1              '9'       9               'Nine'          '' + 'Nine' + ' '
+	 2             '2'       2               'Two'          'Nine ' + 'Two' + ' '
+	 3             '4'       4               'Four'          'Nine Two ' + 'Four' + ' '
+	 4             '7'       7               'Seven'        'Nine Two Four ' + 'Seven' + ' '
+'''
+
+n=input("Enter a number")
+a=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
+s=''
+for x in n:
+    s=s+a[int(x)]+' '
+print(s)
+
+'''
+Write  a  program  to  print  all  the  rotations  of  the  string
+ 1) Let  input  be     S   P  A   C   E
+                               0   1   2   3   4
+    What  are  the  outputs ?  --->  SPACE
+	                                                  PACES
+									                  ACESP
+												      CESPA
+												      ESPAC
+2) What  are  the  indexes  of  SPACE ?  ---> 0  to  4
+     What  are  the  indexes  of  PACES ?  ---> 1  to  4 , 0  to  0
+     What  are  the  indexes  of  ACESP ?  ---> 2  to  4 , 0  to  1
+     What  are  the  indexes  of  CESPA ?  ---> 3  to  4 , 0  to  2
+     What  are  the  indexes  of  ESPAC ?  ---> 4  to  4 , 0  to  3
+3) What  are  the  indexes  in  general ?  --->  i  to  length - 1   and   0  to  i - 1
+'''
+s=input("Enter a string: ")
+for i in range(len(s)):
+    print(s[i:]+s[:i])
+
+'''
+Write  a  program  to  print  mathematical  table  of  a  number
+Let  input  be  7,
+What  is  the  output ?  --->  7 * 1 = 7
+                       						 7 * 2 = 14
+			  								 7 * 3 = 21
+												 .....
+											 7 * 10 = 70
+'''
+
+n=int(input("Enter input: "))
+for i in range(1,11):
+    print(F'{n} * {i} = {n*i}')
+
+'''
+Write a  program to print following pyramid
+Input: 5
+             A
+            A B
+           A B C
+          A B C D
+         A B C D E
+	   i         ch
+---------------------
+       1         'A'
+	   2         'A'  to  'B'
+	   3         'A'  to  'C'
+	   4         'A'  to  'D'
+	   5         'A'  to  'E'
+'''
+
+n=int(input("Enter no.of lines: "))
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(' ',end='')
+    for j in range(i):
+        ch=chr(65+j)
+        print(ch+' ',end='')
+    print()
+
+'''
+Write  a   program  to  print  roman  equivalent  of  a  number
+1000 -  M
+900  -  CM
+500 -  D
+400 - CD
+100 -   C
+90  -  XC
+50  -  L
+40  -  XL
+10  -  X
+9  -  IX
+5  -  V
+4  -  IV
+1  -  I
+1) What  is  the  output  if  input  is  3878 ? ---> MMMDCCCLXXVIII 
+2) What  is  the  result  of  3878 // 1000 ?  --->  3
+    How  many  M's  are  concatenated  to  the  sting ?  --->  Three  becoz  3878 / 1000  is  3
+    What  is  the  result  of  3878 % 1000 ?  --->  878
+3) What  is  the  result  of  878 // 900 ?  --->  0
+    How  many  CM's  are  concatenated  to  the  string ?  ---> Zero  becoz  878 / 900  is  0
+    What  is  the  result  of  878 % 900 ?  --->  878
+4) What  is  the  result  of  878 // 500 ?  ---> 1
+     How  many  D's  are  concatenated  to  the  string ?  ---> One  becoz  878 / 500  is  1
+     What  is  the  result  of  878 % 500 ?  ---> 378
+      and  so  on
+'''
+
+n=int(input("Enter a number: "))
+s=''
+a={1000:"M",900:"CM",500:"D",400:"CD",100:"C",90:"XC",50:"L",40:"XL",10:"X",9:"IX",5:"V",4:"IV",1:"I"}
+for x in a.keys():
+    c=n//x
+    s=s+(c)*a[x]
+    n=n%x
+print(s)
