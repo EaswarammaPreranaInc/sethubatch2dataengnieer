02c4dae8718ce13590bb80ca1819599da2c7b168
Saikumar1207 <122109532+Saikumar1207@users.noreply.github.com>
2025-09-02 09:04:49 +0530

    Update matrix_mul.py

--- matrix_mul.py
+++ matrix_mul.py
@@ -1,3 +1,10 @@
+'''
+The program takes the order and elements of two matrices as input from the user.
+It checks the condition for multiplication: number of columns of the first matrix must equal the number of rows of the second.
+If valid, it multiplies the matrices using nested loops and stores the result in a new matrix.
+Finally, it displays both input matrices and the resultant matrix.
+'''
+
 row1=int(input("Enter number of rows of 1st matrix: "))
 col1=int(input("Enter number of columns of 1st matrix: "))
 row2=int(input("Enter number of rows of 2nd matrix: "))
@@ -41,4 +48,41 @@ if col1 == row2:
 
 else:
     print("Matrix multiplication not possible (col1 != row2)")
+
+'''
+Output:
+
+Enter number of rows of 1st matrix: 2
+Enter number of columns of 1st matrix: 3
+Enter number of rows of 2nd matrix: 3
+Enter number of columns of 2nd matrix: 2
+
+Enter matrix1[0][0] element: 1
+Enter matrix1[0][1] element: 2
+Enter matrix1[0][2] element: 3
+Enter matrix1[1][0] element: 4
+Enter matrix1[1][1] element: 5
+Enter matrix1[1][2] element: 6
+
+Matrix 1:
+[1, 2, 3]
+[4, 5, 6]
+
+Enter matrix2[0][0] element: 7
+Enter matrix2[0][1] element: 8
+Enter matrix2[1][0] element: 9
+Enter matrix2[1][1] element: 10
+Enter matrix2[2][0] element: 11
+Enter matrix2[2][1] element: 12
+
+Matrix 2:
+[7, 8]
+[9, 10]
+[11, 12]
+
+Resultant Matrix:
+[58, 64]
+[139, 154]
+
+'''
 
