0f85e8fac4a131a0671a5c6d9b958c7dfe88027d
Saikumar1207 <122109532+Saikumar1207@users.noreply.github.com>
2025-09-02 09:00:43 +0530

    Update the pattern.py

--- pattern.py
+++ pattern.py
@@ -1,3 +1,11 @@
+'''
+The program takes an integer input n from the user, which represents the number of rows to be printed.
+For each row i, it generates a sequence of alphabets starting from ‘A’ up to (n - i) characters using chr(65 + j).
+The sequence is then reversed to form the right-hand part of the pattern, ensuring symmetry.
+A space of width (2*i - 1) is added in the middle (except for the first row), which increases with each row, creating a hollow effect.
+Finally, the left sequence, spaces, and right sequence are combined and printed row by row, forming a hollow diamond-like alphabet pattern.
+'''
+
 n=int(input("Enter Number of Rows="))
 for i in range(n):
     left = "".join(chr(65 + j) for j in range(n - i))
@@ -7,3 +15,16 @@ for i in range(n):
         print(left + right[1:]) 
     else:
         print(left + space + right)
+
+'''
+output:
+
+Enter Number of Rows = 5
+
+ABCDEDCBA
+ABCD DCBA
+ABC  CBA
+AB   BA
+A    A
+'''
+
