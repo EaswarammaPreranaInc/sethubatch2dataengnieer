fb6c9273427c643b77b799c7823c8d1a3cb7521b
Saikumar1207 <122109532+Saikumar1207@users.noreply.github.com>
2025-09-02 08:52:54 +0530

    Update the str_reverse.py

--- str_reverse.py
+++ str_reverse.py
@@ -1,3 +1,10 @@
+'''
+The program takes a list of strings as input from the user.
+It uses the Bubble Sort technique with nested loops to compare adjacent strings.
+Whenever a string is greater than the next, they are swapped.
+After all iterations, the list is sorted in ascending (alphabetical) order and displayed.
+'''
+
 a=eval(input("Enter the list of String: "))
 n=len(a)
 for i in range(n):
@@ -5,5 +12,6 @@ for i in range(n):
         if a[j]>a[j+1]:
             a[j],a[j+1] = a[j+1],a[j]
 print("Sorted List of Strings: ",a)
+
 
 
