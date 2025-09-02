f33e457a75fccc9c354f9fb2c3218a33d2d2a027
Kumaraswamy96 <kumarbhairy176@gmail.com>
2025-09-02 14:49:19 +0530

    Update and rename 15-7_Kumara_Swamy_Bairy.py to Non_Sequence_15july2025_Kumara_Swamy_Bairy(116).py

--- Non_Sequence_15july2025_Kumara_Swamy_Bairy(116).py
+++ Non_Sequence_15july2025_Kumara_Swamy_Bairy(116).py
@@ -1,3 +1,4 @@
+#float demo program 
 a = 10.8
 print(a)          # 10.8
 print(type(a))      # <class 'float'>
@@ -13,7 +14,11 @@ print(type(d))         # <class 'float'>
 e = 9.62e-2
 print(e)             # 0.0962
 print(9.8.2)         #  invalid syntax
-[16-07-2025 22:51] Sai Satwik: a = 3 + 4j
+
+print()
+
+#complex demo program
+a = 3 + 4j
 print(a)           # (3+4j)
 print(type(a))        # <class 'complex'>
 print(id(a))      # 545454545
@@ -21,17 +26,24 @@ print(a.real)        # 3.0
 print(a.imag)         # 4.0
 print(type(a.real))    # <class 'float'>
 print(type(a.imag))     # <class 'float'>
-[16-07-2025 22:51] Sai Satwik: a = 6j
+
+print()
+
+#find outputs()
+a = 6j
 print(a)           # 6j
 print(type(a))     # <class 'complex'>
 print(a.real)      # 0.0
 print(a.imag)      # 6.0
-
 print(5 + j6)      # error
 print(3 + 4i)      # error
 print(4 + j)       # error
 print(4 + 1j)      # (4+1j)
 print(4 + 0j)      # (4+0j)
+
+print()
+
+#bool object demo program
 a = True
 print(a)             # True
 print(type(a))   # <class 'bool'>
@@ -39,7 +51,6 @@ print(id(a))    # 9480544
 b = False
 print(b)                 # False
 print(type(b))           # <class 'bool'>
-
 print(True + True)      # 2
 print(True + False)      # 1
 print(False + True)      # 1
@@ -51,6 +62,10 @@ print(True)              # True
 print(False)          # False
 print(true)              # error
 print(false)             # error
+
+pritn()
+
+#Find outputs()
 a = 0O6247
 print(a)        # 3255
 print(type(a))     # <class 'int'>
@@ -62,7 +77,11 @@ c = 3239
 print(c)        # 3239
 print(id(c))       # 848768578527
 print(0o9248)     # error
-Sai Satwik: a = 0xA7B9
+
+print()
+
+#find outputs()
+a = 0xA7B9
 print(a)      # 42937
 print(type(a))   # <class 'int'>
 b = 0xBEEF
@@ -72,3 +91,11 @@ print('A7B9')    # A7B9
 print(0XBEER)    # error
 print(0XHYD)  # error
 print(0xA7G9B)    # error
+
+print()
+
+#Find Outputs
+a=9248
+print(a)#9248
+print(type(a))#<class 'int'>
+
