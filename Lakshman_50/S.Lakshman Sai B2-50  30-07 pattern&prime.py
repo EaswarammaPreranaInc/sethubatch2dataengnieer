948de34fafb727ce04ccc790fcf7859093d3a668
SirigiriLakshmanSai <lakshmansirigiri7@gmail.com>
2025-09-03 12:48:13 +0530

    new_patt_added

--- Lakshman_50/S.Lakshman Sai B2-50  30-07 pattern&prime.py
+++ Lakshman_50/S.Lakshman Sai B2-50  30-07 pattern&prime.py
@@ -1,52 +1,52 @@
-'''
+"""
 Write  a  program  to  print  full  pyramid
-	 *
+         *
    ***
   *****
  *******
 *********
 Input  is  number  of  lines
-'''
+"""
 
 # equilateral triangle with odd number of stars
-n=int(input("Enter number of lines: "))
-for i in range(n+1):
-   for _ in range(i+1):
-      print(" "*(n-i), end='')
-      print('*'*(2*i-1))
-      break
-   '''
+n = int(input("Enter number of lines: "))
+for i in range(n + 1):
+    for _ in range(i + 1):
+        print(" " * (n - i), end="")
+        print("*" * (2 * i - 1))
+        break
+    """
     *
    ***
   *****
  *******
 *********
-'''
+"""
 
-# equilateral triangle with odd number of stars with reverse 
-n=int(input("Enter number of lines: "))
-for i in range(n+1):
-   for _ in range(i+1):
-      print(" "*(i), end='')
-      print('*'*(2*n-2*i-1))
-      break
-   '''Enter number of lines: 5
+# equilateral triangle with odd number of stars with reverse
+n = int(input("Enter number of lines: "))
+for i in range(n + 1):
+    for _ in range(i + 1):
+        print(" " * (i), end="")
+        print("*" * (2 * n - 2 * i - 1))
+        break
+    """Enter number of lines: 5
 *********
  *******
   *****
    ***
     *
-      '''
+      """
 
 
 # equilateral triangle with odd number of stars with natural numbers of stars
-n=int(input("Enter number of lines: "))
-for i in range(n+1):
-   for _ in range(i+1):
-      print(" "*(n-i), end='')
-      print('* '*i)
-      break
-   '''
+n = int(input("Enter number of lines: "))
+for i in range(n + 1):
+    for _ in range(i + 1):
+        print(" " * (n - i), end="")
+        print("* " * i)
+        break
+    """
    Enter number of lines: 5
      
     *
@@ -54,37 +54,34 @@ for i in range(n+1):
   * * *
  * * * *
 * * * * *
-'''
+"""
 
 
 # equilateral triangle with odd number of stars with natural numbers of stars with reverse
-n=int(input("Enter number of lines: "))
-for i in range(n+1):
-   for _ in range(i+1):
-      print(" "*i, end='')
-      print('* '*(n-i))
-      break
-'''
+n = int(input("Enter number of lines: "))
+for i in range(n + 1):
+    for _ in range(i + 1):
+        print(" " * i, end="")
+        print("* " * (n - i))
+        break
+"""
 Enter number of lines: 5
 * * * * * 
  * * * *
   * * *
    * *
     *
-    '''
-
-
-
+    """
 
 
 # right angle triangle from left to right with natural numbers of '*'
-n=int(input("Enter number of lines: "))
-for i in range(n+1):
-   for _ in range(i+1):
-      print(" "*(n-i), end='')
-      print('*'*(i))
-      break
-   '''
+n = int(input("Enter number of lines: "))
+for i in range(n + 1):
+    for _ in range(i + 1):
+        print(" " * (n - i), end="")
+        print("*" * (i))
+        break
+    """
    Enter number of lines: 5
      
     *
@@ -92,32 +89,32 @@ for i in range(n+1):
   ***
  ****
 *****
-'''
+"""
 
-# right angle triangle from left to right with natural numbers of '*' in reverse 
-n=int(input("Enter number of lines: "))
-for i in range(n+1):
-   for _ in range(i+1):
-      print(" "*(i), end='')
-      print('*'*(n-i))
-      break
-'''
+# right angle triangle from left to right with natural numbers of '*' in reverse
+n = int(input("Enter number of lines: "))
+for i in range(n + 1):
+    for _ in range(i + 1):
+        print(" " * (i), end="")
+        print("*" * (n - i))
+        break
+"""
 Enter number of lines: 5
 *****
  ****
   ***
    **
     *
-    '''
+    """
 
 # right angle triangle from left to right  with odd no.of '*'
-n=int(input("Enter number of lines: "))
-for i in range(n+1):
-   for _ in range(i+1):
-      print(" "*(2*n-2*i),end='')
-      print('*'*(2*i-1))
-      break
-   '''
+n = int(input("Enter number of lines: "))
+for i in range(n + 1):
+    for _ in range(i + 1):
+        print(" " * (2 * n - 2 * i), end="")
+        print("*" * (2 * i - 1))
+        break
+    """
    Enter number of lines: 5
 
         *
@@ -125,37 +122,32 @@ for i in range(n+1):
     *****
   *******
 *********
-'''
+"""
 
 # right angle triangle from left to right  with odd no.of '*' with reverse
-n=int(input("Enter number of lines: "))
-for i in range(n+1):
-   for _ in range(i+1):
-      print(" "*(2*i),end='')
-      print('*'*(2*n-2*i-1))
-      break
-'''
+n = int(input("Enter number of lines: "))
+for i in range(n + 1):
+    for _ in range(i + 1):
+        print(" " * (2 * i), end="")
+        print("*" * (2 * n - 2 * i - 1))
+        break
+"""
 Enter number of lines: 5
 *********
   *******
     *****
       ***
         *
-        '''
-
-
-
-
-
+        """
 
 
 # right angle triangle from  right to left  with natural number of '*'
-n=int(input("Enter number of lines: "))
-for i in range(n+1):
-   for _ in range(i+1):
-      print('*'*(i))
-      break
-   '''
+n = int(input("Enter number of lines: "))
+for i in range(n + 1):
+    for _ in range(i + 1):
+        print("*" * (i))
+        break
+    """
    Enter number of lines: 5
 
 *
@@ -163,34 +155,32 @@ for i in range(n+1):
 ***
 ****
 *****
-'''
+"""
 
 
-
-# right angle triangle from  right to left  with natural number of '*' in reverse 
-n=int(input("Enter number of lines: "))
-for i in range(n+1):
-   for _ in range(i+1):
-      print('*'*(n-i))
-      break
-   '''
+# right angle triangle from  right to left  with natural number of '*' in reverse
+n = int(input("Enter number of lines: "))
+for i in range(n + 1):
+    for _ in range(i + 1):
+        print("*" * (n - i))
+        break
+    """
    Enter number of lines: 5
 *****
 ****
 ***
 **
 *
-'''
-
+"""
 
 
 # right angle triangle from  right to left  with odd number of '*'
-n=int(input("Enter number of lines: "))
-for i in range(n+1):
-   for _ in range(i+1):
-      print('*'*(2*i-1))
-      break
-   '''
+n = int(input("Enter number of lines: "))
+for i in range(n + 1):
+    for _ in range(i + 1):
+        print("*" * (2 * i - 1))
+        break
+    """
    Enter number of lines: 5
 
 *
@@ -198,60 +188,105 @@ for i in range(n+1):
 *****
 *******
 *********
-'''
+"""
 
 
 # right angle triangle from  right to left  with odd number of '*'
-n=int(input("Enter number of lines: "))
-for i in range(n+1):
-   for _ in range(i+1):
-      print('*'*(2*n-2*i-1))
-      break
-   '''
+n = int(input("Enter number of lines: "))
+for i in range(n + 1):
+    for _ in range(i + 1):
+        print("*" * (2 * n - 2 * i - 1))
+        break
+    """
    Enter number of lines: 5
 *********
 *******
 *****
 ***
 *
-'''
+"""
+
+
+# pattern program
+n = int(input("enter no.of lines: "))
+for i in range(n):
+    for j in range(n - i):
+        print(chr(65 + j), end="")
+    if i > 0:
+        print(" " * (2 * i - 1), end="")
+    for j in range(n - i - 1, -1, -1):
+        print(chr(65 + j), end="")
+    print()
+
+"""
+Output: enter no.of lines: 6
+ABCDEFFEDCBA
+ABCDE  EDCBA
+ABCD    DCBA
+ABC      CBA
+AB        BA
+A          A
+"""
+
 
 import math
-a=[]
-i=1
-num=list(input("enter: "))
-for i in range(1,num[i]):
-   if num[i]%i==0:
-      a.append(i)
-      print(a)
-#print(math.gcd())
+
+a = []
+i = 1
+num = list(input("enter: "))
+for i in range(1, num[i]):
+    if num[i] % i == 0:
+        a.append(i)
+        print(a)
+# print(math.gcd())
 
 
-num=int(input("enter: "))
-a={1:"hundred", 0:None}
-b={1:'ten', 2:'twenty',3:'Thirty',4:'forty',5:'fifty',6:'sixty',7:'seventy',8:'eighty',9:'ninty', 0:None}
-c={1:'one', 2:'tw0',3:'Three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine', 0:None}
-n=int(num//100)
-m=int((num-(n*100))//10)
-p=int(num-((n*100)+(m*10)))
+num = int(input("enter: "))
+a = {1: "hundred", 0: None}
+b = {
+    1: "ten",
+    2: "twenty",
+    3: "Thirty",
+    4: "forty",
+    5: "fifty",
+    6: "sixty",
+    7: "seventy",
+    8: "eighty",
+    9: "ninty",
+    0: None,
+}
+c = {
+    1: "one",
+    2: "tw0",
+    3: "Three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    0: None,
+}
+n = int(num // 100)
+m = int((num - (n * 100)) // 10)
+p = int(num - ((n * 100) + (m * 10)))
 print(n)
 print(m)
 print(p)
-if n*100>100:
-   print(c[n],a[1],b[m],c[p])
+if n * 100 > 100:
+    print(c[n], a[1], b[m], c[p])
 else:
-   print(b[m],c[p])
+    print(b[m], c[p])
 
 
-num=int(input("enter your number: "))
-prime_list=[]
-for i in range(2,num+1):
-   is_prime=True
-   for j in range(2,i):
-      if i%j==0:
-         is_prime=False
-         break
-   if is_prime:
-      prime_list.append(i)
+num = int(input("enter your number: "))
+prime_list = []
+for i in range(2, num + 1):
+    is_prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+            break
+    if is_prime:
+        prime_list.append(i)
 print(prime_list)
-
