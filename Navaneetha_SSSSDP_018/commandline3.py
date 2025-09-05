4cd1e7044b3af469d71719a9591dfbcc5bb65e93
bashettinavaneetha <navaneetha9848@gmail.com>
2025-09-05 19:02:22 +0530

    Add files via upload

---
+++ Navaneetha_SSSSDP_018/commandline3.py
@@ -0,0 +1,28 @@
+'''
+Write  a  program  to  determine  average  of  command  line  inputs
+
+1) py   prog4.py   10.8   25   True   14.6   19   False   7.4
+    What  is  argv ?  --->['prog4.py' , '10.8' , '25' , 'True' , '14.6' , '19' , 'False' , '7.4']
+    What  is  list  'a'  ?  ---> 	[10.8 , 25 , True , 14.6 , 19 , False , 7.4]
+	How  to  determine  sum  of  list  elements ?  ---> sum(a)
+    How  to  determine  number  of  list  elements ?  ---> len(a)
+
+2) py   prog4.py
+    What  is  the  output ?  ---> Pls  send  number  inputs
+
+3) py   prog4.py  25   'Ten'
+    What  is  the  output  ?  --->  Pls  send  number  inputs
+'''
+import sys
+
+args = sys.argv[1:]  # skip script name
+
+if len(args) == 0:
+    print("Pls send number inputs")
+else:
+    try:
+        nums = [float(x) for x in args]  # convert all to numbers
+        avg = sum(nums) / len(nums)
+        print("Average:", avg)
+    except ValueError:
+        print("Pls send number inputs")
