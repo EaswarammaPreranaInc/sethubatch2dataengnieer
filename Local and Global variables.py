1799e6aa1aacd14cebcc570f61abe96f624d3f6c
bhuvansrikar15 <93045723+bhuvansrikar15@users.noreply.github.com>
2025-09-08 21:10:39 +0530

    Create Local and Global variables.py

---
+++ Local and Global variables.py
@@ -0,0 +1,43 @@
+#Find  outputs (Home  work)
+a = 10
+def   f1():
+	b = 40
+	print('a : ' , a) # a : 11
+	print('b : ' , b) # b : 40
+	print('c : ' , c) # c : 3
+	print()
+# End  of  f1()  function
+b = 20
+def    f2():
+	a = 50
+	print('a : ' , a) # a : 50
+	print('b : ' , b) # b : 22
+	print('c : ' , c) # c : 32
+# End  of  f2()  function
+c = 30
+print('a : ' , a) # a : 10
+print('b : ' , b) # b : 20
+print('c : ' , c) # c : 30
+print()
+a +=  1
+b +=  1
+c +=  1
+f1()
+a +=  1
+b +=  1
+c +=  1
+f2()
+print('Bye') # Bye
+
+
+# Find  outputs (Home  work)
+def   f1():
+	a = 20
+	print(a) # 20
+	a += 1
+#End  of  the  function
+a = 10
+print(a) # 10
+a += 1 
+f1()
+print(a) # 11
