68fdd93c94ee895a50991b24e447305554ec1276
kedarisu <kedarisuprabhath7@gmail.com>
2025-09-08 20:02:14 +0530

    first commit

---
+++ python/K Suprabhath_SSSSDP-II-112(30)_Dt.py
@@ -0,0 +1,21 @@
+def gcd(m, n):
+    if n == 0:          # base case
+        return m
+    else:
+        return gcd(n, m % n)   # recursive call
+
+# Example usage
+m = int(input('Enter any number : '))
+n = int(input('Enter any number : '))
+print('Gcd :', gcd(m, n))
+
+
+def sod(n):
+    if n == 0:               # base case
+        return 0
+    else:                    # recursive case
+        return (n % 10) + sod(n // 10)
+
+# Example usage
+n = int(input('Enter any number : '))
+print('Sum of the digits :', sod(n))
