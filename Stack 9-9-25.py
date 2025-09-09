0b9f24ec2bae900b44e09d793e9c273f65a9da2c
bhuvansrikar15 <93045723+bhuvansrikar15@users.noreply.github.com>
2025-09-09 20:56:15 +0530

    Create Stack 9-9-25.py

---
+++ Stack 9-9-25.py
@@ -0,0 +1,110 @@
+#  Tricky  program
+#   Find  outputs
+def  f1():
+	global  a
+	if  a:
+		print(a)
+		a = a - 1
+		f1()
+		print('Hello')
+		print('Hi')
+		print(a)
+	print('Bye')
+# End  of  the  function
+a = 3
+f1()
+print('End')
+
+'''
+3
+2
+1
+Bye
+Hello
+Hi
+0
+Hello
+Hi
+0
+Hello
+Hi
+0
+Bye 
+End
+'''
+
+#   Find  outputs
+def  f1():
+	a = 3
+	if  a:
+		print(a)
+		a = a - 1
+		f1()
+		print('Hello')
+		print('Hi')
+		print(a)
+	print('Bye')
+#End  of  the  function
+a = 3
+f1()
+print('End')
+
+'''
+3
+3
+3
+3
+3
+3
+3
+upto infinite recursion
+'''
+
+
+#  Most  tricky   program
+# Find  outputs  (Home  work)
+def  f1(x , y):
+	if   x > 40:
+		return
+	x += y
+	f1(x , y)
+	print(x)
+#End  of  the  function
+x = 10
+f1(x , x := x + 1) # f1(10 , 11)
+print(x)
+
+'''
+43
+32
+21
+11
+'''
+
+
+# Find  outputs   (Home  work)
+def  f1(x):
+	print(x)
+	if   x:
+		f1(x - 1)
+	print(x)
+# End of the function
+f1(3)
+
+'''
+3
+2
+1
+0
+0
+1
+2
+3
+'''
+
+
+
+
+
+
+'''
