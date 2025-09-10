e46c68a66d6d24010f5a6ea92418ed500b2a3821
Akshay Reddy <poddukuriakshayreddy1605@gmail.com>
2025-09-10 14:04:15 +0530

    recursion_nested functions

--- PoddukuriAkshayReddy_Batch_2_SSSSDP_088/1009_recursion_disk_problem.py
+++ PoddukuriAkshayReddy_Batch_2_SSSSDP_088/1009_recursion_disk_problem.py
@@ -33,3 +33,191 @@ How many disks ? : 3
 2   --->  3
 1   --->  3
 '''
+
+
+
+#  Find  outputs  (Home  work)
+def  outer():
+	x = 10
+	def  inner():
+		nonlocal  x
+		print(x)
+		x = 20
+		print(x)
+		x += 5
+	# End  of  inner  function
+	print(x)
+	x += 5
+	inner()
+	print(x)
+# End  of  outer  function
+outer()
+print(x)
+
+'''
+10
+15
+20
+25
+10
+'''
+
+    
+#  Find  outputs  (Home  work)
+def  outer():
+	x = 10
+	def  inner():
+		print(x) # error due x is not defined in inner function
+		nonlocal  x  
+		x = 20
+		print(x)
+		x += 5
+	# End  of  inner  function
+	print(x)
+	x += 5
+	inner()
+	print(x)
+# End  of  outer  function
+outer()
+
+
+#  Find   outputs(Home  work)
+def  outer():
+	x = 10
+	def  inner():
+		global   x
+		x = 20
+		print(x)
+		x += 5
+	# End  of  inner  function
+	print(x)
+	x += 5
+	inner()
+	print(x)
+# End  of  outer  function
+outer()
+print(x)
+
+'''
+10
+20
+15
+25
+
+'''
+    
+# Find  outputs(Home  work)
+def  outer():
+	def  inner():
+		nonlocal  x # error  due to x is not defined in outer function
+		x = 20
+		print(x)
+	# End  of  inner  function
+	inner()
+	print(x)
+# End  of  the  function
+outer()
+print(x)
+    
+    
+    
+# Find  outputs(Home  work)
+def  outer():
+	def  inner():
+		global   x
+		x = 20
+		print(x)
+		x = x + 5
+	# End  of  inner  function
+	inner()
+	print(x)
+# End  of  the  function
+outer()
+print(x)
+'''
+20
+25
+25
+
+'''
+    
+#  Identify  Error
+def   f1():
+        nonlocal   x # error  due to x is not defined in f1 function
+
+    
+    
+# Find  outputs (Home  work)
+def  outer():
+	a = 10
+	b = 20
+	def   inner():
+		nonlocal   a
+		a = 100
+		b = 200
+		print(a , b)
+	# End  of  inner  function
+	print(a , b)
+	inner()
+	print(a , b)
+#end of outer function
+outer()
+'''
+10 20
+100 200
+100 20
+'''
+
+    
+    
+# Find  outputs (Home  work)
+def   f1():
+	x = 'John'
+	def  f2():
+		nonlocal  x
+		x =  'Hello'
+	#end of inner function
+	f2()
+	return  x
+#  End  of  f1()  function
+print(f1())
+'''
+Hello
+'''
+    
+    
+    # Find  output(Home  work)
+def  fun():
+	x = 10
+	def    gun():
+		x =  x +  20 # error due to x is not defined in gun function or nonlocal or global
+		print(x)
+	#end of inner function
+	gun()
+#end of outer function
+fun()
+
+    
+    
+    
+#  Identify  Error
+x = 10
+def   outer():
+	x = 20
+	def  inner():
+		global   x
+		nonlocal  x # x is nonlocal and global at a time
+
+    
+#  Find  outputs  (Home   work)
+def   f1():
+	x = 10
+	def  f2():
+		nonlocal   x
+		def  f3():
+			nonlocal   x
+			print(x)
+		f3()
+	f2()
+f1()
+# 10
