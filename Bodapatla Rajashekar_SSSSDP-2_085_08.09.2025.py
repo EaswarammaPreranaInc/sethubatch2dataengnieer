3037269761673ea1ffe3b64e7b0f85cc79cc99cd
shekarbodapatla <shekarbodapatla22@gmail.com>
2025-09-11 19:39:51 +0530

    Add files via upload

---
+++ Bodapatla Rajashekar_SSSSDP-2_085_08.09.2025.py
@@ -0,0 +1,29 @@
+#1. Write  a  recursive  function  to  determine  gcd (or) hcf  of  2 numbers
+def  gcd(m , n):
+	if  n == 0:
+		return  m
+	else:
+		return gcd(n, m % n) 
+'''
+1) gcd(4 , 6)  =
+'''
+m = int(input('Enter  any  number  :  '))
+n = int(input('Enter  any  number  :  '))
+print('Gcd : ',gcd(m, n))
+
+
+
+#2. Write  a  recursive  function  to  find  sum of  the  digits  of  a  number
+def   sod(n):
+	if  n > 0:
+		return  (n % 10) + sod(n // 10)
+	else:
+		return 0
+'''
+1) sod(9427) =
+'''
+n = int(input('Enter  any  number :   '))
+print('Sum of the digits : ', sod(n))
+
+
+
