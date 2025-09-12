4a28f20d960ae1ed17812ae771133bb454abe4b6
SaiPraneeth <126940106+Katakamsaipraneeth@users.noreply.github.com>
2025-09-12 16:52:47 +0530

    Add files via upload

---
+++ katakamsaipraneeth_SSSSDP-II-106_12-09-2025.py
@@ -0,0 +1,40 @@
+# Find  outputs  (Home  work)
+def  square(fun): # fun = num
+	def  inner1():
+		x = fun() # x = num() # x = 10
+		return  x * x # 100
+	return  inner1 # inner1
+def   double(fun): # fun = inner1
+	def  inner2():
+		y = fun() # y = inner1() # y = 100
+		return  2 * y # 200
+	return   inner2 # inner2
+@double # num = double(square(num)) # num = double(inner1) # num = inner2
+@square
+def  num():
+	return  10
+#end of the function
+print(num()) # print(inner2()) # 200
+
+
+
+# Find  outputs  (Home  work)
+def   bold(fun): # inner2
+	def  inner1():  
+		return  '<b>'+  fun()  +'</b>' # '<b>'+  inner2()  +'</b>' # '<b>'+'<i>'+'<u>'+ 'Hello World' +'</u>'+'</i>'  +'</b>'
+	return  inner1
+def   italic(fun): # fun = inner3
+	def   inner2():
+		return  '<i>'  +  fun() +  '</i>' # '<i>'+  inner3 +'</i>' # '<i>'+'<u>'+  'Hello World'  +'</u>' +'</i>'
+	return  inner2 # inner2
+def   underline(fun): # fun = f1
+	def   inner3():
+		return  '<u>'  +  fun()  +  '</u>' # '<u>'  +  f1  +  '</u>' # '<u>'  +  'Hello World'  +  '</u>'
+	return  inner3 # inner3
+@bold # f1 = bold(italic(underline(f1))) # f1 = bold(italic(inner3)) # f1 = bold(inner2) # f1 = inner1
+@italic
+@underline
+def   f1():
+       return  'Hello  World'
+# End  of  the  function
+print(f1()) # print(inner1()) # '<b>'+'<i>'+'<u>'+  'Hello World'  +'</u>'+'</i>'+'</b>'
