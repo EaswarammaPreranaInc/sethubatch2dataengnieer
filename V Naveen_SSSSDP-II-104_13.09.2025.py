3246ac28e103396c39c3406beb2e44ae0c20baa9
VARIKOLLA NAVEEN <147158048+naveen9550@users.noreply.github.com>
2025-09-15 19:06:34 +0530

    V Naveen_SSSSDP-II-104_13.09.2025.py

---
+++ V Naveen_SSSSDP-II-104_13.09.2025.py
@@ -0,0 +1,384 @@
+#1.  How  to  iterate  generator  with  for  loop
+import  time
+def   f1():
+	print('One')
+	yield  25
+	print('Two')
+	yield  10.8
+	print('Three')
+	yield  'Hyd'
+	print('Four')
+# End  of  generator
+g = f1()
+for   x   in   g:
+	print(x)
+	time . sleep(1)
+	print('Hello')
+# End  of  for  loop
+print('End')
+print(g)
+print(next(g))
+g = f1()
+print(next(g))
+
+'''
+One
+25
+Hello
+Two
+10.8
+Hello
+Three
+Hyd
+Four
+End
+< generator object f1 at 0x000385128  >
+Error
+One 
+25
+'''
+
+
+
+
+#2. Most  tricky  program
+# Find  outputs(Home  work)
+import  time
+def   f1():
+	yield  25
+	yield  10.8
+	yield  'Hyd'
+# End  of  generator
+g = f1()
+print(next(g))
+for  x  in   g:
+	print(x)
+print()
+for  x  in   f1():
+	print(x)
+print()
+gen = f1()
+print(next(gen))
+for  x  in   f1():
+	print(x)
+print(next(gen))
+
+
+'''
+25
+10.8
+Hyd
+
+25
+10.8
+Hyd
+
+25
+25
+10.8
+Hyd
+10.8
+
+'''
+
+
+
+
+
+
+#3.Find  outputs (Home  work)
+import  time
+g = (x * x   for    x    in    range(5))
+for  y  in   g:
+	print(y)
+	time . sleep(2)
+	print('Hello')
+for  y  in   g:
+	print(y)
+
+
+'''
+0
+Hello
+1
+Hello
+4
+Hello
+9
+Hello
+16
+Hello
+'''
+	
+
+
+
+
+#4. Find  outputs (Home  work)
+import  time
+for  y  in   (x * x   for    x    in    range(5)):
+	print(y)
+	time . sleep(2)
+for  y  in   (x * x   for    x    in    range(5)):
+	print(y)
+	time . sleep(2)
+	
+
+'''
+0
+1
+4
+9
+16
+0
+1
+4
+9
+16
+'''
+
+
+
+
+
+#5. Find  outputs(Home  work)
+import  time
+g1 = (x * x   for  x  in  range(5))
+g2 = g1
+for  y  in  g1:
+	print(y)
+	time . sleep(2)
+for  y  in  g2:
+	print(y)
+print(g1  is  g2)
+
+
+'''
+0
+1
+4
+9
+16
+True
+'''
+
+
+
+
+
+#6.  Find  outputs (Home  work)
+l = [x * x   for   x   in   range(5)]
+print(l)
+print(type(l))
+
+s = {x * x   for   x   in   range(5)}
+print(s)
+print(type(s))
+
+d = {x : x * x    for   x   in   range(5)}
+print(d)
+print(type(d))
+
+g = (x * x   for   x   in   range(5))
+print(g)
+print(type(g))
+
+
+'''
+[ 0 1 4 9 16 ]
+< class 'list' >
+{ 0 1 4 9 16 }
+< class 'set' >
+{ 0:0 1:1 2:4 3:9 4:16 }
+< class 'dict' >
+< generator object <genexpr> at 0x00000482448 >
+< class 'generator' >
+'''
+
+
+
+
+
+
+#7.  Find  outputs (Home  work)
+def  f1():
+	return  10
+	return  20
+	return  30
+def  f2():
+	yield  10
+	yield  20
+	yield  30
+# End  of  the  function
+print(f1())
+print(f1())
+print(f1())
+print()
+g = f2()
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+
+
+'''
+10
+10
+10
+
+10
+20 
+30
+Stop iteration Error
+'''
+
+
+
+
+
+
+#8.  Prove  that  there  is  no  waiting  time  for  generator
+from  timeit  import   timeit
+print(timeit('[x * x   for  x  in  range(500) ]'))
+print(timeit('( x * x   for  x  in  range(500) )'))
+
+
+'''
+22.41761130397208
+0.2752650090260431
+'''
+
+
+
+
+
+
+
+#9. Prove  that  there  is  no  memory  error  for  generator
+import  sys
+list = [x * x   for   x   in    range(10000)]
+gen = (x * x   for   x   in    range(100000000000000000000000000000000000000000000000))
+print(sys . getsizeof(list))
+print(sys . getsizeof(gen))
+
+
+'''
+85176
+200
+'''
+
+
+
+
+'''10.
+Write  a  generator  to  yield  sum , difference , product  and  division  of  2  numbers
+
+Hint:  Use  generator  function  and  for  loop  to  iterate  elements
+'''
+def  f1(a,b):
+	print("sum  : " , end='')
+	yield  a+b
+	print("Difference : " , end = '')
+	yield  a-b
+	print("Product : " , end = '')
+	yield  a*b
+	print("Division : " , end = '')
+	yield  a/b
+a = int(input("Enter first nuumber : "))
+b = int(input("Enter second number : "))
+g = f1(a,b)
+for   x   in   g:
+	print(x)
+
+'''
+Enter   first  number  :   10
+Enter   second  number  :   7
+Sum : 17
+Differnece :  3
+Product :  70
+Division : 1.4285714285714286
+'''
+
+
+
+
+
+
+
+
+
+'''11.
+Design  a  generator  to  yield  from  x (may  be  10)  to   y (may  be  20)
+
+Hint:  Use  generator  function  and  for  loop
+
+Hint:  Do  not  use  range  object
+'''
+
+def  f1(a,b):
+	yield x
+a = int(input("Enter start value : "))
+b = int(input("Enter end value : "))
+g = f1(a,b)
+for   x   in   range(a,b+1):
+	print(x)
+
+
+'''
+Enter  start  value :  10
+Enter  end  value :  20
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+'''
+
+
+
+
+
+
+
+'''12.
+Write  a   generator  to  generate  fibonacci  series
+
+1) What  is  fibonacci  series ?  --->  0 , 1 , 1 , 2 , 3 , 5 , 8 , .....
+
+2) What  is  the  formula  for  10th  term ?  --->  9th  term + 8th  term
+    What  is  the  formula  for  3rd  term ?  ---> 2nd  term + 1st  term
+
+3) What  are  the  first  two  terms ?  --->  0  and  1
+
+4) Use  generator  function  and  for  loop
+'''
+
+
+def fibonacci_series(n):
+    yield a
+n = int(input("Enter the last value of fibonacci series : "))
+g = fibonacci_series(n)
+a, b = 0, 1
+for _ in range(n):
+    if a <= n :
+        print(a)
+        a, b = b, a + b
+print('End')
+
+'''
+Enter the last value of fibonacci series:10
+0
+1
+1
+2
+3
+5
+8
+End
+'''
