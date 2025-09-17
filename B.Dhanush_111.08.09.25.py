304bd4497b4aac25c15c4c6455fce4556bca376a
Dhanush03102001 <dhanushbeeram03@gmail.com>
2025-09-17 17:06:47 +0530

    B.Dhanush_111.08.09.25.py

---
+++ B.Dhanush_111.08.09.25.py
@@ -0,0 +1,56 @@
+''' 1) Write  a  recursive  function  to  determine  gcd (or) hcf  of  2 numbers
+
+1) gcd(12 , 15) =  gcd(15 , 12)  = gcd(12 , 3) = gcd(3 ,  0) =  3
+
+2) gcd(4 , 7) =  gcd(7 , 4)  = gcd(4 , 3) = gcd(3 , 1) = gcd(1 , 0) = 1
+'''
+def  gcd(m , n):
+	if  n>0:
+		return  gcd(n,m%n)
+	else:
+		return   m
+        
+m = int(input('Enter  any  number  :  '))
+n = int(input('Enter  any  number  :  '))
+print('Gcd :  ' ,   gcd(m,n))
+
+
+'''
+# Output:
+Enter  any  number  :  12
+Enter  any  number  :  15
+Gcd :   3
+'''
+
+
+
+
+''' 2) Write  a  recursive  function  to  find  sum of  the  digits  of  a  number
+
+sod(678) =  678 % 10 + sod(678 // 10)
+              =  8 + sod(67)
+              =  8 + 67 % 10 + sod(67 // 10)
+              =  8 + 7 + sod(6)
+              =  8 + 7 + 6 % 10 + sod(6 // 10)
+              =  8 + 7 + 6 + sod(0)
+              =  8 + 7 + 6 + 0
+			  = 21
+
+1) How  many  function  calls  are  in  sod(678) ?  --->  4
+
+2) How  many  function  calls  are  in  sod(n-digit  number) ?  ---> n + 1
+'''
+def   sod(n):
+	if  n>0:
+		return  n%10+sod(n//10)
+	else:
+		return  0
+        
+n = int(input('Enter  any  number :   '))
+print('Sum of the digits :   ' , sod(n))
+
+'''
+# Output:
+Enter  any  number :   9427
+Sum of the digits :    22
+'''
