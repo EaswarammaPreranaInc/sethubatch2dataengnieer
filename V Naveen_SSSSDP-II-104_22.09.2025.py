ff1e591a023117943950106075410f939a7c3068
VARIKOLLA NAVEEN <147158048+naveen9550@users.noreply.github.com>
2025-09-22 17:06:55 +0530

    V Naveen_SSSSDP-II-104_22.09.2025.py

---
+++ V Naveen_SSSSDP-II-104_22.09.2025.py
@@ -0,0 +1,104 @@
+#1. Save  in  any  file  of  cwd
+import  Prog1 . mod1
+print(Prog1.mod1.x) # How  to  print  object  'x'  of  mod1  in  package  p1
+Prog1.mod1.f1() # How  to  call  function  f1()  of  mod1  in  package  p1
+a = Prog1.mod1.c1()
+a.m1() # How  to  call  method  m1()  of  class  c1  in  mod1  of  package  p1
+print()
+print()
+print(Prog1.x) # How  to  print  object  'x'  of  _init_  module  in  package  p1
+Prog1.f1() # How  to  call  function  f1()  of  _init_  module  in  package  p1
+a = Prog1.c1()
+a.m1() # How  to  call  method  m1()  of  class  c1  in   init  module  of  package  p1
+
+#20
+#p1  --->  mod1   --->  f1  function
+#p1  ---> mod1  ---> c1  ---> m1 method
+
+
+#10
+#package  p1 ---> _init_  module ---> f1  function
+#package  p1 ---> _init_  module ---> class  c1  ---> m1  method
+
+
+
+#2. Save  in  any  file  of  cwd
+from  Prog1   import  mod1
+print(mod1.x) # How  to  print  object  'x'  of  mod1  in  package  p1
+mod1.f1() # How  to  call  function  f1()  of  mod1  in  package  p1
+a = mod1.c1()
+a.m1() # How  to  call  method  m1()  of  class  c1  in  mod1  of  package  p1
+# print(p1 . x) # Error
+# print(p1 . _init_ . x) # Error
+# print(_init_ . x) # Error
+# _init_   module  of  package  Prog1  is  executed
+#10
+#p1  --->  mod1   --->  f1  function
+#p1  ---> mod1  ---> c1  ---> m1 method
+
+
+
+#3. Save  in  any  file  of  cwd
+from  Prog1 . mod1   import  *
+print(x) # How  to  print  object  'x'  of  mod1  in  package  p1
+f1() # How  to  call  function  f1()  of  mod1  in  package  p1
+a = c1() 
+a.m1() # How  to  call  method  m1()  of  class  c1  in  mod1  of  package  p1
+#print(p1 . x) # Error
+#print(p1 . _init_ . x) # Error
+#print(_init_ . x) # Error
+#from  p1  import  mod1 . * # Error
+#_init_   module  of  package  Prog1  is  executed
+#10
+#p1  --->  mod1   --->  f1  function
+#p1  ---> mod1  ---> c1  ---> m1 method
+
+
+
+#4. Save  in  any  file  of  cwd
+from  Prog1 . mod1   import  *
+print(x) # How  to  print  object  'x'  of  mod1  in  package  p1
+f1() # How  to  call  function  f1()  of  mod1  in  package  p1
+a = c1()
+a.m1() # How  to  call  method  m1()  of  class  c1  in  mod1  of  package  p1
+#print(p1 . x) # Error
+#print(p1 . _init_ . x) # Error 
+#print(_init_ . x) # Error
+#from  p1  import  mod1 . * # Error
+#_init_   module  of  package  Prog1  is  executed
+#10
+#p1  --->  mod1   --->  f1  function
+#p1  ---> mod1  ---> c1  ---> m1 method
+
+
+
+#5. Save  in  any  file  of  cwd
+import Prog1.__init__  # How  to  import  _init_  module  of  package  p1  with  import  statement
+print(Prog1.__init__ .x) #How  to  print  object  'x'  of   _init_  module   in   package  p1
+Prog1.__init__.f1() # How  to  call  function  f1()  of   init  module  in  package  p1
+a = Prog1.__init__.c1() 
+a.m1() # How  to  call method  m1()  of  class  c1  in   init  module  of  package  p1
+print(Prog1.x) # How  to  print  object  'x'  of   _init_  module   in   package  p1  in  another  way
+Prog1.f1() # How  to  call  function  f1()  of   _init_  module  in  package  p1  in  another  way
+a = Prog1.c1()
+a.m1() # How  to  call  method  m1()  of  class  c1  in   _init_  module  of  package  p1  in  another  way
+# print(p1 . mod1 . x) # Error
+#_init_   module  of  package  Prog1  is  executed
+#_init_   module  of  package  Prog1.__init__  is  executed
+#10
+#package  p1 ---> _init_  module ---> f1  function
+#package  p1 ---> _init_  module ---> class  c1  ---> m1  method
+#10
+#package  p1 ---> _init_  module ---> f1  function
+#package  p1 ---> _init_  module ---> class  c1  ---> m1  method
+
+
+
+#6. Save  in  any  file  of  cwd
+import   Prog1
+import  Prog1 . mod1
+from   Prog1   import  mod1
+from   Prog1 . mod1  import   *
+import  Prog1 . __init__
+#_init_   module  of  package  Prog1  is  executed
+#_init_   module  of  package  Prog1.__init__  is  executed
