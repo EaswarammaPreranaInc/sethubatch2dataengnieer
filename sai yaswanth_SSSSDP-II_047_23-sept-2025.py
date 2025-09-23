1eebaabf93fc8b23e04a859168a5d7c3cf807585
sai yaswanth chithirala <151661511+sai2403@users.noreply.github.com>
2025-09-23 14:33:14 +0530

    Add files via upload

---
+++ sai yaswanth_SSSSDP-II_047_23-sept-2025.py
@@ -0,0 +1,149 @@
+
+# Identify  error  (Home work)
+class   c1:
+	def  m1(self):
+		pass
+class   c2:
+        pass
+class   c3: # Error
+
+# Find  outputs  (Home  work)
+class   c1:
+    pass
+# End  of  the  class
+a = c1()
+print(id(a)) #address of a
+print(type(a)) # class '__main__.c1'
+print(a . __dict__) # convert  to  dictionary -->{}
+print(a) #type  and  address of a
+del  a #a is deleted
+print(a) # error:a is not defined
+
+#  Find  outputs  (Home  work)
+def   m1():
+        print('Function')
+class   c1:
+    def   m1(self):
+        print('1st  method')
+    def   m1(self):
+        print('2nd  method')
+    def   m1(self):
+        print('3rd  method')
+# End  of  class  c1
+a = c1()
+a . m1() # 1st  method
+m1() # Function
+
+#  Find  outputs  (Home  work)
+class   c1:
+    def   m1(self):
+        print('No  argument  method')
+    def   m1(self , x):
+        print('Single  argument  method : ' , x)
+    def   m1(self , x , y):
+        print('Two  argument  method : ' , x , y)
+# End  of  class  c1
+a = c1()
+a . m1(10 , 20) #  Two  argument  method  :  10  20
+a . m1(30) # error: arg y is missing
+a . m1() # error: arg x , y  are missing
+
+#  Find  outputs  (Home  work)
+class   c1:
+    def   m1(self):
+        print('No  argument  method')
+    def   m1(self , x):
+        print('Single  argument  method : ' , x)
+    def   m1(self , x = 1  , y = 2):
+        print('Two  argument  method : ' , x , y)
+# End  of  class  c1
+a = c1()
+a . m1(10 , 20) #  Two  argument  method  :  10  20
+a . m1(30) # Two  argument  method  :  30  2
+a . m1() #  Two  argument  method  :  1  2
+
+# Find  outputs  (Home  work)
+class   c1:
+    def   m1(self):
+        print('Method  of  first  c1  class')
+class   c1:
+    def   m1(self):
+        print('Method  of  second  c1  class')
+class   c1:
+    def   m1(self):
+        print('Method  of  third  c1  class')
+a = c1()
+a . m1() # Method  of  third  c1  class
+
+# Find  outputs  (Home  work)
+class   c1:
+    def   m1(self):
+        print('Method  of  first  c1  class')
+class   c1:
+    def   m1(self):
+        print('Method  of  second  c1  class')
+class   c1:
+    pass
+a = c1()
+a . m1() # AttributeError: 'c1'  object  has  no  method  'm1'
+
+#  Find  outputs (Home  work)
+class  c1:
+        pass
+# End  of  class
+a = c1()
+print(a . __dict__)  #  empty  dictionary
+a . x = 10
+print(a .__dict__) #{'x':10}
+a . y = 20
+print(a . __dict__) #{'x':10,'y':20}
+a . x = 30
+print(a . __dict__) #{'x':30,'y':20}
+a . y = 40
+print(a . __dict__) #{'x':30,'y':40}
+del  a . x
+print(a . __dict__) #{'y':40}
+del  a . y
+print(a . __dict__) # {}
+del   a
+print(a . __dict__) #'a' is not defined
+
+
+
+'''  (Home  work)
+Write  a  program  to  determine  area  and  perimeter  of  triangle  and  represent  triangle  by  an  object
+
+1) What  is  the  area  of  triangle ?  --->  sqrt(s * (s - a) * (s - b) * (s - c))
+
+2) What  is  the  formula  for  's' ?  ---> (a + b + c) / 2
+
+3) What  is  the  perimeter  of  triangle ?  --->  a + b + c
+'''
+import math
+class triangle:
+    def get_sides(self):
+        self.x = float(input("Enter the first side: "))
+        self.y = float(input("Enter the second side: "))
+        self.z = float(input("Enter the third side: "))
+
+    def test(self):
+        if (self.x + self.y > self.z) and (self.y + self.z > self.x) and (self.x + self.z > self.y):
+            return True
+        else:
+            print("Not a triangle")
+            exit()
+
+    def area(self):
+        s = (self.x + self.y + self.z) / 2
+        return math.sqrt(s * (s - self.x) * (s - self.y) * (s - self.z))
+
+    def peri(self):
+        return self.x + self.y + self.z
+a = triangle()
+a.get_sides()
+a.test()
+print("Area:", a.area())
+print("Perimeter:", a.peri())
+
+
+
