683debc293556db926c95b5ebbe09ca5305ce94d
DhanushRaj9 <dhanushraj9@gmail.com>
2025-09-23 14:34:23 +0530

    Add files via upload

--- 21aug.py
+++ 21aug.py
@@ -1,49 +1,65 @@
 1.
+# Find  outputs   (Home  work)
 a = 25 , 10.8 , 3 + 4j , 'Hyd' , True , None , 'Hyd' , 25
-print(a) #(25 , 10.8 , (3 + 4j) , 'Hyd' , True , None , 'Hyd' , 25)
-print(type(a))  #<class 'tuple'>
-a[3] = 'Sec'   #error, tuple is immutable
-a[3 : 6] = 60 , 70 , 80   #error, tuple is immutable
+print(a) # (25 , 10.8 , (3 + 4j) , 'Hyd' , True , None , 'Hyd' , 25)
+print(type(a)) # class <'tuple'>
+a[3] = 'Sec' # Error-Tuple is immutable
+a[3 : 6] = 60 , 70 , 80 # Error because slice is not allowed in tuple
+
+
 
 2.
+
+#  Find  outputs
 a = (1,2,3)
 b = (4,5,6)
-print(a , id(a))  #(1,2,3)  address of (1,2,3)
-a += b  #a=a+b = a=(1,2,3)+(4,5,6) = (1,2,3,4,5,6)
-print(a , id(a)) #(1,2,3,4,5,6) address of (1,2,3,4,5,6)
+print(a , id(a)) # (1,2,3)  145952665268
+a += b
+print(a , id(a)) # (1,2,3,4,5,6) 14066526925
+
 
 3.
+
+
+#  Find  outputs
 a = (1,2,3)
 b = (4,5,6)
-print(a , id(a))  #(1,2,3)  address of (1,2,3)
-a = a + b    #a=a+b = a=(1,2,3)+(4,5,6) = (1,2,3,4,5,6)
-print(a , id(a))  #(1,2,3,4,5,6) address of (1,2,3,4,5,6)
+print(a , id(a)) # (1,2,3)  14685626529
+a = a + b
+print(a , id(a)) # (1,2,3,4,5,6)  146265292289
 
 
 4.
-a = input('Enter  Tuple  :  ')  #input function converts tuple into string
-print(a)  #(10 , 20 , 30 , 40) now is a string
-print(type(a))  #<class 'str'>  
-b = eval(a)   #converts string to tuple 
-print(b)   #(10 , 20 , 30 , 40)  is now a tuple
-print(type(b))   #<class 'tuple'>
-print(len(b))  #4  which is (10 , 20 , 30 , 40)
 
+#  What   are  the  outputs  if  input  is  (10 , 20 , 30 , 40) ?   (Home  work)
+a = input('Enter  Tuple  :  ')
+print(a) # (10,20,30,40)
+print(type(a)) # class<'str'>
+b = eval(a)
+print(b) # (10,20,30,40)
+print(type(b)) # class<'tuple'>
+print(len(b)) # 4
 
 5.
-a = (10 , [20 , 30 , 40] , 50 , 60)  #tuple with a list inside it
+
+# Find  outputs  (Home  work)
+a = (10 , [20 , 30 , 40] , 50 , 60)
 a[1][0] = 70
-print(a)  #(10 , [70 , 30 , 40] , 50 , 60)
-a[1] = [80 , 90 , 100] #element in tuple is immutable we get error
-print(a)  #error
+print(a) #  (10 , [70 , 30 , 40] , 50 , 60)
+a[1] = [80 , 90 , 100]
+print(a) # Error 
 
 
 6.
+
+# Find  outputs  (Home  work)
 a = [10 , (20 , 30 , 40) , 50 , 60]
 a[1][0] = 70
-print(a)  #error, tuple is immutable
-a[1] = [80 , 90]
-print(a)  #[10 ,[80 , 90] , 50 , 60]
+print(a) # Error (Tuple is immutable)
+a[1] = [80 , 90]
+print(a) # Error
+
+
 
 
 7.
@@ -53,66 +69,72 @@ b = 10.8
 c = 'Hyd'
 d = True
 x = a , b , c , d
-print(x) #(25, 10.8, 'Hyd', True) #In Python, writing values separated by commas automatically creates a tuple.
-print(type(x))  #<class 'tuple'>
+print(x) # (25,10.8,'Hyd',True)
+print(type(x)) # class<'tuple'>
+
 
 
 8.
+
 # Find  outputs   (Home  work)
-x = 25 , 10.8 , 'Hyd' , True  #In Python, writing values separated by commas automatically creates a tuple.
+x = 25 , 10.8 , 'Hyd' , True
 a , b , c , d = x
-print(a) #25
-print(b)  #10.8
-print(c)  #'Hyd'
-print(d)  #True
-p , q , r =  x #error x has 4 values but we gave only 3
-a , b , c , d  , e = x #error x has 4 values but we gave 5
+print(a) # 25  
+print(b) # 10.8
+print(c) # Hyd
+print(d) # True
+p , q , r =  x # Error x has 4 elements
+a , b , c , d  , e = x # Error
 
 
-9. 
+
+
+9.
 # Find  outputs   (Home  work)
-x = 25 , 10.8 , 'Hyd' , True  #In Python, writing values separated by commas automatically creates a tuple.
+x = 25 , 10.8 , 'Hyd' , True
 a , *b , c = x
-print(a) #25
-print(b)  #[10.8, 'Hyd']  *b collects items as list
-print(c)  #True
+print(a) # 25
+print(b) # [10.8,'Hyd']
+print(c) # True	
+
 
 
 10.
-tpl = 25 , 10.8 , 'Hyd' , True  #it is a tuple as it is with commas and is a group of elements
-a , b , *c , d , e = tpl #there are only 4 elements in tpl, but we have got 5 to assign
-print(a)  #25
-print(b)  #10.8
-print(c)  #[] empty list as we don't have any other element to assign to *c
-print(d)  #'Hyd'
-print(e)  #True
+# Find  outputs   (Home  work)
+tpl = 25 , 10.8 , 'Hyd' , True
+a , b , *c , d , e = tpl
+print(a) # Error
+print(b) # Error
+print(c) # Error
+print(d) # Error
+print(e) # Error
 
 
 11.
+
+
 # Find  outputs   (Home  work)
-x = 25 , 10.8 , 'Hyd' , True , 3 + 4j #is a tuple
-a , b , _ , d , _= x 
-print(a)  #25
-print(b)  #10.8
-print(_)  #(3+4j)  #Hyd will be overwritten as _ is present twice 
-print(d)  #True
-print(_)  #(3+4j)  #if _ is appeared again, the latest value will overwrite
+x = 25 , 10.8 , 'Hyd' , True , 3 + 4j
+a , b , _ , d , _= x
+print(a) # 25 
+print(b) # 10.8 
+print(_) # (3+4j)
+print(d) # True
+print(_) # (3+4j)
+
+
 
 
 12.
 # tuple()  function  demo  program   (Home  work)
-a = range(100 , 150 , 10)  # 100 110  120  130  140
-b = tuple(a)  #tuple(100 110  120  130  140)
-print(b) #(100, 110, 120, 130, 140)
-print(type(b)) #<class 'tuple'>
-c = [10 , 20 , 15, 18] 
-d = tuple(c)  
-print(d) #(10, 20, 15, 18)
-e = tuple('Vamsi')  
-print(e) #('V', 'a', 'm', 's', 'i')
-print(tuple(25)) #error
-print(tuple()) #()
-
-
-
-
+a = range(100 , 150 , 10)
+b = tuple(a)
+print(b) # (100,110,120,130,140)
+print(type(b)) # class<'tuple'>
+c = [10 , 20 , 15, 18]
+d = tuple(c)
+print(d) # (10,20,15,18)
+e = tuple('Vamsi')
+print(e) # ('V','a','m','s','i')
+print(tuple(25)) # Error
+print(tuple()) # ()
--- 22aug.py
+++ 22aug.py
@@ -5,180 +5,240 @@ a = (10 , 20 , 15 , 12 , 14 , 15 , 18 , 19 , 15 , 12 , 25)
 try:
 	i = a . index(15)
 	while  True:
-		print('15 is found at index : ' , i) #15 is found at index :2<next line>15 is found at index :5<nextline>
-                                                        15 is found at index : 8 <next line>
-		i = a . index(15 , i + 1)     
+		print('15 is found at index : ' , i)
+		i = a . index(15 , i + 1)
 except:
-		print(F'15  is  found  {a . count(15)}  times')  #15  is  found 3 times
+		print(F'15  is  found  {a . count(15)}  times')
+		
+'''
+output:-
+15 is found at index :  2
+15 is found at index :  5
+15 is found at index :  8
+15  is  found  3  times
+'''
 
-2.
-#  How  to  modify  an  element  of  tuple ?    (Home  work)
-a  =  10 ,  20 ,  30 ,   40 ,  50
-#     0      1       2       3      4
-a[2] = 35   #error: item assignment is not possible in tuple because tuple is immutable
-print(a)   #(10,20,30,40,50)
-print(id(a)) #address of tuple
-#How  to  modify  30  in  tuple  to  35 --> using slicing and concatenate method
-a=(a[:2])+(35,)+(a[3:])  #using slicing of tuple and then concatenating a new element 35
-print(a)
-print(id(a)) #address of new tuple
+
+
+
+
+2.a = (10, 20, 30, 40, 50)
+print("Before:", a, id(a))
+
+a = a[:2] + (35,) + a[3:]   # replace index 2 (30) with 35
+
+print("After:", a, id(a))
+
+
+
 
 
 3.
+
 # How  to  delete  an  element  of  tuple ?   (Home  work)
 a  = 10 , 20 , 30 , 40 , 50
 #    0     1      2     3      4
-a . remove(30) #we get error as tuple doesn't support item deletion as it is immutable
-del  a[2]   #we get error as tuple doesn't support item deletion as it is immutable
-a . pop(2) #we get error as tuple doesn't support item deletion as it is immutable
-print(a)   #(10 , 20 , 30 , 40 , 50)
-print(id(a))  #address of tuple
-#How  to  remove  30  from  tuple  'a'--> using slice method and concatenation
-a=a=(a[:2])+(a[3:5])
-print(a)   #(10, 20, 40, 50)
-print(id(a))   #133004532434240
+a . remove(30) # Error
+del  a[2] # Error
+a . pop(2) # Error
+print(a)
+print(id(a))
+How  to  remove  30  from  tuple  'a'
+print(a)
+print(id(a))
+
+'''
+4.
+a = (10, 20, 30, 40, 50)
+print("Before:", a, id(a))
+
+temp = list(a)    # convert to list
+temp.remove(30)   # remove element
+a = tuple(temp)   # convert back to tuple
+
+print("After:", a, id(a))
+'''
+
+
 
 
 4.
-#  Nested   tuple  (Home  work)
-a = ( (10 , 20)  ,  (30 , 40 , 50)  ,  (60 , 70 , 80 , 90) )
-print(a)  #( (10 , 20)  ,  (30 , 40 , 50)  ,  (60 , 70 , 80 , 90) )
-print(type(a))  #<class 'tuple'>
-print(len(a))   #3
-print(How  to  print  1st  inner  tuple) #a[0]
-print(How  to  print  2nd  inner  tuple)  #a[1]
-print(How  to  print  3rd  inner  tuple)  #a[2]
-print(How  to  print  20)   #a[0][1]
-print(How  to  print  50)   #a[1][2]
-print(How  to  print  90)   #a[2][3]
+a = ((10, 20), (30, 40, 50), (60, 70, 80, 90))
+print(a)             # whole tuple
+print(type(a))       # <class 'tuple'>
+print(len(a))        # 3 -> there are 3 inner tuples
+
+# Access inner tuples
+print(a[0])          # 1st inner tuple -> (10, 20)
+print(a[1])          # 2nd inner tuple -> (30, 40, 50)
+print(a[2])          # 3rd inner tuple -> (60, 70, 80, 90)
+
+# Access specific elements
+print(a[0][1])       # 20  → 1st tuple, index 1
+print(a[1][2])       # 50  → 2nd tuple, index 2
+print(a[2][3])       # 90  → 3rd tuple, index 3
+
+
+
+
 
 
 5.
-# Find  outputs  (Home  work)
-a = ((10 , 20 , 30),)
-print(How  to   print  inner  tuple)  #print(a[0])
-print(How  to   print  inner  tuple  in  another  way)  #print(a[-1])
-print(How   to  print   10)  #print(a[0][0])
-print(How   to  print   20)  #print(a[0][1])
-print(How   to  print   30)  #print(a[0][2])
+a = ((10, 20, 30),)
+
+# print inner tuple
+print(a[0])         # (10, 20, 30)
+print(a[-1])        # (10, 20, 30) -> another way
+
+# print individual elements
+print(a[0][0])      # 10
+print(a[0][1])      # 20
+print(a[0][2])      # 30
+
+
 b = ((),)
-print(How  to   print  inner  tuple  of  tuple  'b')  #print(a[0])
-print(How  to   print  inner  tuple  of  tuple  'b'  in  another  way)  #print(a[-1])
+
+# print inner tuple of 'b'
+print(b[0])         # ()
+print(b[-1])        # () -> another way
+
+
 
 
 
 6.
+
 #  Find  outputs (Home  work)
 a = ((10 , 20 , 30))
-print(a)  #(10,20,30)
-print(*a)  #unpacks tuple 10 20 30
+print(a) # (10, 20, 30)
+print(*a) # 10 20 30
 b = (())
-print(b)  #()
-print(*b)  #unpacks b we get empty line as there is nothing inside the tuple(empty tuple)
+print(b) # ()
+print(*b) # nothing is printed because tuple is empty
+
+
+
 
 
 7.
 # What  are  the  outputs  if  input  is  {10 , 20 , 15 , 18 , 20 , 12 , 18}
-a = input('Enter  Set  :  ')  #giving values {10 , 20 , 15 , 18 , 20 , 12 , 18}
-print(a)  #input function will create any given value to string hence {10 , 20 , 15 , 18 , 20 , 12 , 18} becomes string
-print(type(a)) #<class 'str'>
-b = eval(a)  #eval({10 , 20 , 15 , 18 , 20 , 12 , 18})--> eval of string {10 , 20 , 15 , 18 , 20 , 12 , 18} eval function turns string {10 , 20 , 15 , 18 , 20 , 12 , 18} to respective obj that is set
-print(b)  #{10 , 20 , 15 , 18 , 20 , 12 , 18}
-print(type(b))  #<class 'set'>
+a = input('Enter  Set  :  ')
+print(a) # {10 , 20 , 15 , 18 , 20 , 12 , 18}
+print(type(a)) # <class 'str'>
+b = eval(a)
+print(b) # {18, 20, 10, 12, 15}
+print(type(b)) # <class 'set'>
+
+
+
 
 
 8.
 #  Find  outputs  (Home  work)
-print({(10 , 20 , 30)})  #{(10,20,30)}
-print({[10 , 20 , 30]})  #error lists are mutable
-print({{10 , 20 , 30}})  # error sets are mutable
-print({{}})  #dictionary is mutable
+print({(10 , 20 , 30)}) # {(10, 20, 30)}
+print({[10 , 20 , 30]}) # Error
+print({{10 , 20 , 30}}) # Error
+print({{}}) # Error
+
+
+
+
+
+->Direct print using print()
+print("Set with print function:")
+print(a)
+->Iterate elements of set with for loop
+print("Iterate elements of set with for loop:")
+for element in a:
+    print(element)
+->Print using unpacking (*)
+print("Set with unpacking (*):")
+print(*a)
+->Print using join() (works only if all elements are strings)
+b = {"apple", "banana", "kiwi"}
+print("Set with join:")
+print(", ".join(b))
+
+
+
 
 
 9.
-# How  to  print  set  in  differnet ways  (Home  work)
-a = {25 , True , 'Hyd' , 10.8}
-print('set  with  print  function')
-print(a)
-print('Iterate  elements  of  set  with  for  loop')
-How  to  iterate  set  with  for  loop
-
-
-10.
-a = {25 , True , 'Hyd' , 10.8}
-print('set  with  print  function')
-print(a) #{25, True, 'Hyd', 10.8}
-print('Iterate  elements  of  set  with  for  loop')
-How  to  iterate  set  with  for  loop   #for x in a:
-						print(x) 
-#25
-10.8
-Hyd
-True  we get set elements in unordered as set is unordered
-
-
-11.
 # Find  outputs  (Home  work)
 a = 'Hyd'
 b = True
 c = 25
 d = 1
 e = 'Hyd'
-s = {a , b , c , d , e}  
-print(s)  #{25, True, 'Hyd'}  1 and 'Hyd' are ignored because we already have True and 'Hyd', set ignores duplicates
-print(len(s))  #3
-print(type(s))  #<class 'set'>
+s = {a , b , c , d , e}
+print(s) # {'Hyd', True, 25}
+print(len(s)) # 3
+print(type(s)) # <class 'set'>
+
+
+
+
+
+10.
+# Find  outputs  (Home  work)
+s = {'Hyd',  25,  True,  10.8 }
+print(s) # {25, 10.8, 'Hyd', True}
+a , b , c , d = s
+print(a) # 25
+print(b) # 10.8
+print(c) # Hyd
+print(d) # True
+
+
+
+
+11.
+# Find  outputs  (Home  work)
+s = {'Hyd',  25,  True,  10.8 }
+print(s) # {25, 10.8, 'Hyd', True}
+a , *b = s
+print(a) # 25
+print(b) # [10.8, 'Hyd', True]
+print(type(b)) # <class 'list'>
+
+
+
 
 12.
 # Find  outputs  (Home  work)
 s = {'Hyd',  25,  True,  10.8 }
-print(s)   #{'Hyd',  25,  True,  10.8 }not sure if we get elements in same order
-a , b , c , d = s
-print(a)  #'Hyd'
-print(b)  #'25'
-print(c)  #True
-print(d)  #10.8
+print(s) # {25, 'Hyd', 10.8, True}
+a , *b , c = s
+print(a) # 25
+print(b) # ['Hyd', 10.8]
+print(c) # True
+
+
 
 
 13.
-# Find  outputs  (Home  work)
-s = {'Hyd',  25,  True,  10.8 }
-print(s)  #unordered elements {'Hyd', 10.8, 25, True}
-a, *b = s #{'Hyd', [10.8, 25, True]}
-print(a) Hyd
-print(type(a)) #<class 'str'>
-print(b)   #[10.8, 25, True]
-print(type(b))  #<class 'list'>
-
-
-
-14.
-# Find  outputs  (Home  work)
-s = {'Hyd',  25,  True,  10.8 }
-print(s) #{'Hyd',  25,  True,  10.8 } can be in different format like 25 in first place and True in second
-a , *b , c = s  #{'Hyd',[25, True],10.8}
-print(a)  #Hyd
-print(b)  #[25, True]
-print(c)  #10.8
-
-
-15.
+1.# Find  outputs  (Home  work)
 s = {20 , 10 , 20 , 10}
-print(s)  #{20,10} removes duplicates and may not be in same order
-x , y = s  #20,10 = s
-print(x)  #20
-print(y)  #10
+print(s) # {10, 20}
+x , y = s
+print(x) # 10
+print(y) # 20
 
 
-# set()  function  demo  program  (Home  work)
-a = range(100 , 151 , 10)  #100 110 120 130 140 150
-b = set(a)  #set(100 110 120 130 140 150)-->{100, 110, 120, 130, 140, 150}
-print(b)  #{100, 110, 120, 130, 140, 150} can not expect in same order because set is unordered
+
+
+14
+a = range(100 , 151 , 10)
+b = set(a)
+print(b) # {130, 100, 140, 110, 150, 120}
 c = [10 , 20 , 15 , 18 , 10 , 50 , 20 , 12 , 18]
-d = set(c)  #set([10 , 20 , 15 , 18 , 10 , 50 , 20 , 12 , 18])-->removed duplicated and we get ordered {10, 20, 15, 18, 12, 50}
-print(d)  #{10, 20, 15, 18, 12, 50} could be different because it is unordered
-e = set('Rama  rAo') 
-print(e) #{'a', 'R', 'm', ' ', 'A', 'o', 'r'} not in ordered
-print(set(25))  #error, set of sequence is not possible
-print(set())  #set()
+d = set(c)
+print(d) # {10, 12, 15, 18, 50, 20}
+e = set('Rama  rAo')
+print(e) # {'R', 'o', 'm', 'r', 'a', ' ', 'A'}
+print(set(25)) # Error
+print(set()) # set()
+
+
+
 
