4caf05dff2965b2cc5717f00e148ca9ff6138528
DhanushRaj9 <dhanushraj9@gmail.com>
2025-09-23 14:26:31 +0530

    Add files via upload

--- 23aug.py
+++ 23aug.py
@@ -1,53 +1,62 @@
-1. 
-# add()  method  demo  program  (Home  work)
+1.# add() method demo program
 a = set()
-a . add(True) #(True,)
-a . add(25)
-a . add(10.8)
-a . add(1)
-a . add('Hyd')
-a . add(25)
-a . add(None)
-a . add('Hyd')
-a . add(1.0)
-print(a)   #{True, 10.8, 25, 'Hyd', None}we get set of elements in unordered, set removes duplicates
-a . add(10 , 20 , 30) #error, add cannot add multiple elemts at a time 
-a . add([10,20,30]) #error, list is mutable hence it will not fit in set
+
+a.add(True)       # Adds boolean True
+a.add(25)         # Adds integer 25
+a.add(10.8)       # Adds float 10.8
+a.add(1)          # Adds integer 1 but True is same as 1, so won't add new element)
+a.add('Hyd')      # Adds string "Hyd"
+a.add(25)         # Duplicate (ignored)
+a.add(None)       # Adds None
+a.add('Hyd')      # Duplicate (ignored)
+a.add(1.0)        # 1.0 is equal to 1 -> won't add new element
+
+print(a)
+
+a.add(10, 20, 30)     # Error -> add() takes exactly one argument
+a.add([10, 20, 30])   # Error -> list is mutable, cannot be added to set
+
+
 
 
 2.
+
 # Find  outputs  (Home  work)
 a = {25 , 10.8 , 'Hyd' , True}
-tpl = (10 , 20 , 30)
-print(a)  #{25 , 10.8 , 'Hyd' , True}
-print(id(a))  #address of set
-a . add(tpl)  #{True, 'Hyd', 10.8, (10, 20, 30), 25}
-a . add('Sec') #{True, 'Hyd', 'Sec', 10.8, (10, 20, 30), 25}
-print(a)  #{True, 'Hyd', 'Sec', 10.8, (10, 20, 30), 25}
-print(id(a))  #address of set
-print(len(a)) #6
-a . add([100 , 200 , 300])  #error, list is mutable and cannot fit in set
-a . add(set()) #error set
-a . add({ }) #error dict
-
+tpl = (10 , 20 , 30){25, 'Hyd', 10.8, True}
+print(a) # {25, 'Hyd', 10.8, True}
+print(id(a)) # 135969459491488
+a . add(tpl)
+a . add('Sec')
+print(a) # {'Sec', True, 10.8, (10, 20, 30), 'Hyd', 25}
+print(id(a)) # 135969459491488
+print(len(a)) # 6
+a . add([100 , 200 , 300]) # Error
+a . add(set()) # Error
+a . add({ }) # Error
 
 3.
 # Find  outputs (Home  work)
 s = set()
 tpl = (10 , 20 , 15 , 18)
 s . add(tpl)
-print(s)   #{(10 , 20 , 15 , 18)}
-print(len(s))  #1
+print(s) # {(10, 20, 15, 18)}
+print(len(s)) # 1
+
+
 
 
 4.
+
 # update()  method  demo program  (Home  work)
 tpl = (10 , 20 , 15, 18 , 10 , 20)
 s = set()
-s . update(tpl)  #s.update((10 , 20 , 15, 18 , 10 , 20))= set(10 , 20 , 15, 18 , 10 , 20)={10 , 20 , 15, 18}
-print(len(s))   #4
-print(s) # {10 , 20 , 15, 18}
-s . update(25)   #{10 , 20 ,25, 15, 18}
+s . update(tpl)
+print(len(s)) # 4
+print(s) # {10, 18, 20, 15}
+s . update(25) # Error
+
+
 
 
 5.
@@ -55,88 +64,95 @@ s . update(25)   #{10 , 20 ,25, 15, 18}
 a = [10 , 20 , 30]
 b = {30 , 40,50 }
 c = (50 , 60 , 70)
-s = set()   
-s . update(a , b , c)#s=set([10 , 20 , 30],{30 , 40,50 },(50 , 60 , 70) )
-print(s)  #{10,20,30,40,50,60,70}  order may not be same, in any order
-print(len(s)) #7
-s . add(a , b , c)   #error, set.add takes only 1 element, here it has taken 3 elements
+s = set()
+s . update(a , b , c)
+print(s)  # {50, 20, 70, 40, 10, 60, 30}
+print(len(s)) # 7
+s . add(a , b , c) # Error add() takes exactly one argument
 
 
 6.
-# Find  outputs  (Home work)
-a = {10 , 20 , 30 , 40}   #set
-b = [30 , 40 , 50 , 60]   #list
-print(a . union(b))   #{10,20,30,40,50,60} in any order
-print(a | b)  #error:unsupported
-print(b . union(a))  #error: b is list and list do not have union method, 
-print(a + b)  #error: set does not support concatenation
 
-
-7.
 # Find  outputs  (Home  work)
 a = set()
 a . update('Rama Rao')
-print(a) #set('Rama Rao')={'R','a','m',' ','o'} in any order
-print(len(a))  #5
-a . update(3 + 4j , 10.8 , True) #{'R','a','m',' ','o', 3+4j, 10.8, True} in any order
+print(a) #  {'R', 'a', ' ', 'm', 'o'}
+print(len(a)) # 5
+a . update(3 + 4j , 10.8 , True) # Error 'complex' object is not iterable
+
+
+
+
+7.
+# copy()  method  demo  program  (Home  work)
+a = {10 , 20 , 15 , 18}
+print(a) # {10, 18, 20, 15}
+b = a . copy()
+print(b) # {10, 18, 20, 15}
+print(a  is  b) # False
+print(a  ==  b) # True
+c = a
+print(a  is  c) # True
+
+
 
 
 8.
-a = {10 , 20 , 15 , 18}  #set
-print(a)   #{10 , 20 , 15 , 18} in any order
-b = a . copy()  #{20,10,18,15}  in any order shallow copy
-print(b)   #{20,10,18,15}  in any order shallow copy
-print(a  is  b)   #False  #
-print(a  ==  b)   #True   #both sets contains same elements
-c = a  #  c={10 , 20 , 15 , 18}
-print(a  is  c)  #True
-
-
-
-
-9.
+# pop()  method  demo  program  (Home  work)
 a = {25 , 10.8 , 'Hyd' , True}
-print(a)   #{25 , 10.8 , 'Hyd' , True}       #in any order
-print(a . pop())  #1st element will be deleted and returns 1st element in o/p i.e. 25
-print(a . pop())  #  "
-print(a . pop())  #  "
-print(a . pop())  @  "
-print(a . pop())  #error as there are no elemts left, all 4 elements deleted in set and it is an empty set i.e. set()
-print(a)  #set()
+print(a) # {25, 10.8, 'Hyd', True}
+print(a . pop()) # 25
+print(a . pop()) # 10.8
+print(a . pop()) # True
+print(a . pop()) # Hyd
+print(a . pop()) # Error 'pop from an empty set'
+print(a) # set()
 b = {10 , 20 , 30 , 40}
-print(b . pop(2))   #error: if we mention 2 it doesn't delete 2nd argument, pop doesn't take arguments, it directly deleted 1 st element of set
-print(b) # {10 , 20 , 30 , 40}
+print(b . pop(2)) # pop() takes no arguments
+
+
+9
+
+# remove()  method  demo  program  (Home  work)
+a = {25 , 10.8 , 'Hyd' , True}
+print(a) # {25, 10.8, 'Hyd', True}
+a . remove('Hyd'){25, 10.8, True}
+print(a) # {25, 10.8, True}
+a . remove('Sec') # Error no Sec
+
 
 
 
 10.
-
+# discard()  method  demo  program (Home  work)
 a = {25 , 10.8 , 'Hyd' , True}
-print(a)   #{25 , 10.8 , 'Hyd' , True} in any order
-a . remove('Hyd')  #{25 , 10.8 , True}
-print(a)   ##{25 , 10.8 , True}
-a . remove('Sec')  #error, there is no elemnt names Sec in the set
+print(a) # {25, 10.8, 'Hyd', True}
+a . discard('Hyd')
+print(a) # {25, 10.8, True}
+a . discard('Sec')
+print(a) # {25, 10.8, True}
+a . remove('Sec') # Error no Sec
+
+
 
 
 11.
-a = {25 , 10.8 , 'Hyd' , True}
-print(a)   #{25 , 10.8 , 'Hyd' , True} in any order
-a . discard('Hyd')  #{25 , 10.8, True}
-print(a)  ##{25 , 10.8, True}
-a . discard('Sec')  #there is no element names Sec in the set, discard doesn't give error for it, it remains calm
-print(a) #{25 , 10.8, True}
-a . remove('Sec') #error, unlike discard, remove doesn't sit calm even if there is no Sec. It raises error.
+# clear()  method  demo  program (Home  work)
+a = {10 , 20 , 15 , 18}
+print(a) # {10, 18, 20, 15}
+a . clear()
+print(a) # set()
+print(len(a))  # 0 
+
+
+
+
 
 12.
-
-a = {10 , 20 , 15 , 18}
-print(a)   #{10 , 20 , 15 , 18} in any order
-a . clear()   #clears all elements in set makes it empty set
-print(a)  #set()
-print(len(a))  #0
-
-
-
-
-
-
+# Find  outputs  (Home work)
+a = {10 , 20 , 30 , 40}
+b = [30 , 40 , 50 , 60]
+print(a . union(b)) # {40, 10, 50, 20, 60, 30}
+print(a | b) # Error
+print(b . union(a)) # Error 'list' object has no attribute 'union'
+print(a + b) # Error 
