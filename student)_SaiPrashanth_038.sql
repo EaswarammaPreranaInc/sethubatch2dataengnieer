ad28ce0f39e8bdbb047d39962b21ebcf259e91a4
Mashetty Sai Prashanth <136419634+MSaiPrashanth@users.noreply.github.com>
2025-09-25 19:26:14 +0530

    Add files via upload

---
+++ student)_SaiPrashanth_038.sql
@@ -0,0 +1,7 @@
+create database students;
+use students;
+create table student(s_no int, s_name varchar(20), s_pno int);
+
+select * from student;
+insert into student(s_no, s_name , s_pno )
+values(1,'sai',567);
