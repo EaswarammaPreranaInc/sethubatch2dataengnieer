142c4b8821644a890c62260587c13d6233338a7a
shekarbodapatla <shekarbodapatla22@gmail.com>
2025-10-07 17:25:57 +0530

    Add files via upload

---
+++ Bodapatla Rajashekar_SSSSDP-2_085__DSA(07-10-2025).py
@@ -0,0 +1,91 @@
+# Write  a  program  to  implement  deque  using  list
+class  deque:
+	def   __init__(dq):
+			 dq . list = [] # How  to  create  an  empty  queue
+	def  isempty(dq):
+            		return dq.list == [] # return  True  when  deque  is  empty  and  False  otherwise
+	def  ins_rear(dq , x):
+			dq.list.append(x) # How  to  insert  'x'  at  the  end  of  deque
+	def  ins_front(dq , x):
+			dq . list . insert(0 , x) # How  to  insert  'x'  at  the  begining  of  deque
+	def  del_front(dq):
+			try : # How  to  remove  left  most  element  of  the  deque  and  return  the  deleted  element
+				return dq . list . pop(0)
+			except : # (return  None  when  deletion  is  not  possible)
+				return None
+	def  del_rear(dq):
+			try : # How  to  remove  right  most  element  of  the  deque  and  return  the  deleted  element
+				return dq . list . pop()
+			except : # (return  None  when  deletion  is  not  possible)
+				return None
+	def left_most(dq):
+		try:
+			return dq.list[0]
+		except:
+			return None
+	def right_side(dq):
+		try:
+			return dq.list[-1]
+		except:
+			return None
+	def  disp(dq):
+		print(dq.list) # How  to  print  deque
+	def  size(dq):
+		return len(dq.list) # return  number  of  elements  in  the  deque
+#End of the class
+def  menu():
+	print('1. Insert  element  at  the  end  of  deque')
+	print('2. Insert  element  at  the  begining  of  deque')
+	print('3. Delete  left  most  element')
+	print('4. Delete  right  most  element')
+	print('5. Print  Deque')
+	print('6. Print  left  most  element')
+	print('7. Print  right  most  element')
+	print('8. Number  of  elements  in  deque')
+	print('9. Exit')
+#end of  the  function
+dq = deque() # How  to  create  deque  class  object
+while  True:
+	menu()
+	ch = int(input('Enter Choice :   '))
+	match  ch:
+		case  1:
+					x = eval(input('Enter  element  to  be  inserted : '))
+					dq . ins_rear(x) # How  to  insert  'x'  at  the  end  of  deque
+					dq . disp() # How  to  print  deque
+		case  2:
+					x = eval(input('Enter  element  to  be  inserted : '))
+					dq . ins_front(x) # How  to  insert  'x'  at  the  begining  of  deque
+					dq . disp() # How  to  print  deque
+		case  3:
+					x = dq . del_front() # How  to  delete  left  most  element  of  deque  and  print  the  deleted  element
+					if x == None:
+						print('Value deletion is not possible')
+					else: 
+						print(f'Left Most Deleted Value : {x}')
+					dq . disp() # How  to  print  queue
+		case  4:
+					x = dq . del_rear() # How  to  delete  right  most  element  of  deque  and  print  the  deleted  element
+					if x == None:
+						print('Value deletion is not possible')
+					else: 
+						print(f'Right Most Deleted Value : {x}')
+					dq . disp() # How  to  print  queue
+		case  5:
+					dq . disp() # How  to  print  the  queue
+		case  6:
+					x = dq.left_most() # How  to  print  left  most  element  of  deque
+					if x == None:
+						print('No Elements in the Queue')
+					else:
+						print(f'Left Most Element is : {x}')
+		case  7:
+					x = dq . right_most() # How  to  print  right  most  element  of  deque
+					if x == None:
+						print('No Elements in the Queue')
+					else:
+						print(f'Right Most Element is : {x}')
+		case  8:
+					print(dq . size()) # How  to  print  number  of  elements  in  deque
+		case  9:
+					exit() # How  to  stop  execution
