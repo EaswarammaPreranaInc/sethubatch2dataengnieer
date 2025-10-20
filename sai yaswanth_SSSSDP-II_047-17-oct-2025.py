6b99aaee12ea4e64d6b6bc2e7371e383f8525c48
sai yaswanth chithirala <151661511+sai2403@users.noreply.github.com>
2025-10-20 19:21:35 +0530

    Add files via upload

---
+++ sai yaswanth_SSSSDP-II_047-17-oct-2025.py
@@ -0,0 +1,582 @@
+#  Multilevel  inheritance  demo  program
+class  A:
+    def    m1(self):
+        print('class   A  method')
+class  B(A):
+    def  m1(self):
+        print('class  B   method')
+class   C(B):
+    def  m1(self):
+        print('class   C    method')
+class   D(C):
+    def   m1(self):
+        print('class   D   method')
+        super().m1() # How  to  call  method  m1()  of  class  C
+        super(D,self).m1() # How  to  call  method  m1()  of  class  C  in  another  way  without  creating  an  object
+        super(C,self).m1() # How  to  call  method  m1()  of  class  B
+        super(B,self).m1() # How  to  call  method  m1()  of  class  A
+        super(A , self) . m1() #error  because  class  A  is  the  top  most  class
+        super(C) . m1() # super needs 0 or 2 arguments but not 1
+# End  of  the  class
+d=D() # How  to  call  method  m1()  of  class  D
+d.m1() 
+
+# Find  outputs  (Home  work)
+class  father:
+        def  height(self):
+                print('Father  Height')
+class  mother:
+        def  color(self):
+                print('Mother  Color')
+class  child(mother , father):
+        def  qualification(self):
+                print('Child Qualification')
+# End  of  the  class
+c  =  child()
+c . qualification() # Child  Qualification
+c . color() # mother color
+c . height() # Father Height
+c . m1() # error: no method m1 in child class
+
+#  Find  outputs
+class  uncle:
+        def  m1(self):
+                print('Uncle  Method')
+class  mother:
+        def  m1(self):
+                print('Mother  Method')
+class  father:
+        def  m1(self):
+                print('Father  Method')
+class  child(father , mother , uncle):
+        def  m1(self):
+                print('Child  Method')
+#end  of  the  class
+c = child()
+c . m1() # Child  Method
+
+# Find  outputs
+class  uncle:
+        def  m1(self):
+                print('Uncle  Method')
+class  mother:
+        def  m1(self):
+                print('Mother  Method')
+class  father:
+        def  m1(self):
+                print('Father  Method')
+class  child(father , mother , uncle):
+    pass
+#end  of  the  class
+c = child()
+c . m1() # Father  Method
+# Find  outputs
+class  uncle:
+        def  m1(self):
+                print('Uncle  Method')
+class  mother:
+        def  m1(self):
+                print('Mother  Method')
+class  father:
+        pass
+class  child(father , mother , uncle):
+        pass
+#end  of  the  class
+c = child()
+c . m1() # mother method
+
+# Find  outputs
+class  uncle:
+        def  m1(self):
+                print('Uncle  Method')
+class  mother:
+        pass
+class  father:
+        pass
+class  child(father , mother , uncle):
+        pass
+#end  of  the  class
+c = child()
+c . m1() # Uncle  Method
+
+# Find  outputs
+class  uncle:
+        pass
+class  mother:
+        pass
+class  father:
+        pass
+class  child(father , mother , uncle):
+        pass
+#end  of  the  class
+c = child()
+c . m1() #error: no method in child and parent classes
+
+# Find  outputs
+class   father:
+    def  m1(self):
+        print('m1  method  of  Father  class')
+class   mother:
+    def  m1(self):
+        print('m1  method  of  Mother  class')
+class   uncle:
+    def  m1(self):
+        print('m1  method  of  Uncle  class')
+class   child(father , mother , uncle):
+    def  m1(self):
+        print('m1  method  of  child  class')
+        super().m1() # How  to  call  m1()  method  of  father  class
+        super(child,self).m1() # How  to  call  m1()  method  of  father  class  in  another  way  without  creating  an  object
+        super(father,self).m1() # How  to  call  m1()  method  of  mother  class   without  creating  an  object
+        super(mother,self).m1() # How  to  call  m1()  method  of  uncle  class  without  creating  an  object
+        super(uncle , self) . m1() #error:uncle  class  is  the  last  class  in  mro
+# End of the class
+print(child . __mro__) # child-father-mother-uncle-object
+c=child()
+c.m1() # How  to  call  m1()  method  of  child  class
+print('Bye')
+
+# Parent  and  child  class  constructors (Home  work)
+class   parent:
+    def   __init__(self):
+        print('parent  constructor')
+    def   __del__(self):
+        print('parent  destructor')
+class  child(parent):
+    def   __init__(self):
+        super().__init__() # How  to  call  parent  class  constructor
+        print('child   constructor')
+    def   __del__(self):
+        super(child,self).__del__() # How  to  call  parent  class  destructor
+        print('child   destructor')
+# End of the class
+c = child()
+print('Bye')
+
+# Find  outputs  (Home  work)
+class   parent:
+    def   __init__(self):
+        print('parent  constructor')
+    def   __del__(self):
+        print('parent  destructor')
+class  child(parent):
+    def   __init__(self):
+        print('child   constructor')
+    def   __del__(self):
+        print('child  destructor')
+# End of the class
+c = child()
+print('Bye')
+'''
+child   constructor
+Bye
+child  destructor
+'''
+
+# Find  outputs  (Home  work)
+class   parent:
+    def   __init__(self):
+        print('parent  constructor')
+    def   __del__(self):
+        print('parent  destructor')
+class  child(parent):
+    pass
+# End of the class
+c = child()
+print('Bye')
+'''
+parent  constructor
+Bye
+parent  destructor
+'''
+# Parent  and  Child  constructor  demo  program  (Home  work)
+class  parent:
+    def   __init__(self , a1 , b1):
+        self . a = a1
+        self . b = b1
+    def disp(self):
+        print(self . a , self . b , sep = '\t' , end = '\t')
+class   child(parent):
+    def __init__(self , a2 = 0  , b2 = 0 , c2 = 0  , d2 = 0):
+        super().__init__(a2, b2)  # How  to  call  parent  class  constructor  with  a2 , b2
+        self . c = c2
+        self . d = d2
+    def  disp(self):
+        super(child,self).disp() # How  to  call  parent  class  disp()  method
+        print(self . c , self . d , sep = '\t')
+#end of the class
+x = child(10 , 20 , 30 , 40)
+y = child()
+print('Object  x')
+x . disp()
+print('Object  y')
+y . disp()
+
+'''
+Object  'x'  : 10 20 30 40
+
+Object  'y'  : 0 0 0 0
+'''
+
+# Find outputs  (Home  work)
+class  parent:
+    x = 100
+    def   __init__(self):
+        self . x = 10
+class   child(parent):
+    def   __init__(self):
+        super() . __init__()
+        self . y = 20
+    def disp(self):
+        print(parent.x) # How  to  print  static  variable  'x'
+        print(super().x) # How  to  print  static  variable  'x'   in  another  way
+        print(child.x) # How  to  print  static  variable  'x'   in  one  more  way
+        print(self.x) # How  to  print  variable  'x'  of  object  'c'
+        print(self.y) # How  to  print  variable  'y'  of  object  'c'
+#end of the class
+c=child() # How  to  call  disp()  method  of   child  class
+c.disp()
+
+
+# Find  outputs
+class  parent:
+    x = 10
+    def  __init__(self):
+        self . x = 20
+class   child(parent):
+    def  __init__(self):
+        self . x = 30
+        print(self . x)
+        super() . __init__()
+    def  disp(self):
+        print(self . x)
+        print(super() . x)
+# End of the class
+c = child()
+c . disp()
+
+'''
+10
+20
+30
+'''
+
+# Find outputs*
+class    parent:
+    a = 10 # How  to  add  static  variable  'a'  to  parent  class  with  value  10
+    def     __init__(self):
+        print('Parent  constructor')
+        self.x = 30 # How  to  add  instance  variable  'x'  with  value  30
+    def   m1(self):
+        print('Parent  class  instance  method  :  ' ,  self.x ) # How  to  print  variable  'x'
+    @classmethod
+    def    m2(cls):
+        print('Parent  class  "class"  method  :  ' ,  cls.a ) # How  to  print  static  variable  'a'
+        print('Parent  class  "class"  method  :  ' ,  parent.a )# How  to  print  static  variable  'a'  in  another  way
+        print(self . a) #error
+    @staticmethod
+    def   m3():
+        print('Parent  class  static  method  :  ' ,  parent.a) # How  to  print  static  variable  'a'
+    def   _del_(self):
+        print('parent  destructor  :  ' ,  self.x) #How  to  print  variable  'x'
+class  child(parent):
+    b = 20 # How  to  add  static  variable  'b'  with  value  20
+    def   __init__(self):
+        super().__init__() # How  to  call  parent  class  constructor
+        print('Child  constructor')
+        self.y = 40 # How  to  add  instance  variable  'y'  with  value  40
+    def   m1(self):
+        super().m1 # How  to  call  m1()  method  of  parent  class
+        print('Child  class  instance  method')
+        print(self.y) # How  to  print  variable  'y'
+    @classmethod
+    def   m2(cls):
+        super(parent,cls).m2() # How  to  call  m2()  method  of  parent  class
+        parent.m2() # How  to  call  m2()  method  of  parent  class  in  another  way  without  creating  an  object
+        cls . m2() # may be recursive call
+        self . m2() # error
+        print('Child  class  "class"  method')
+        print(child.a) # How  to  print  static  variable  'a'
+        print(parent.a) # How  to  print  static  variable  'a'  in  another  way
+        print(cls.a) # How  to  print  static  variable  'a'  in  one  more  way
+        print(super().a) # How  to  print  static  variable  'a'  in  last  way
+        print(child.b) # How  to  print  static  variable  'b'
+        print(cls.b) # How  to  print  static  variable  'b'  in  another  way
+    @staticmethod
+    def   m3():
+        parent.m3() # How  to  call  m3()  method  of  parent  class
+        #super().m3() # How  to  call  m3()  method  of  parent  class  in   another  way
+        self . m3()
+        cls . m3() # error
+        print('child  class  static  method' , cls.a) # How  to  print  static  variable  'a'
+        print(child.a) # How  to  print  static  variable  'a'  in  another  way
+        print(child.b) # How  to  print  static  variable  'b'
+    def _del_(self):
+        super().__del__() # How  to  call  destructor  of  parent  class
+        print('child  destructor' ,  self.y) # How  to  print  variable  'y'
+#end of the class
+child.m2 # How  to  call  m2()  method  of  child  class
+child.m3() # How  to  call  m3()  method  of  child  class
+c=child()
+c.m1() # How  to  call  m1()  method  of  child  class
+
+
+'''
+Write  a  funciton  to  concatenate  two  linked  lists
+
+How  to  concatenate  two  linked list's ?  ---> Modify  last  node  link  of  1st  linked list  to  first  node  of  2nd  linked list
+'''
+from prog2 import *
+class  sll(linked_list):
+    def  concat(a , b):
+        if  a.isempty():
+            a.first=b.first     # 2nd  linked  list  is  the   result
+        else:
+            last=a.first
+            while last.link!=None:
+                last=last.link
+            last.link=b.first   # How  to  attach  last  node  of  1st  LL  with  first  node  of  2nd  LL
+#  End  of  the  class
+a=sll()
+a.create()  # How  to  create  1st  LL
+b=sll()
+b.create()  # How  to  create  2nd  LL
+a.concat(b) # How  to  concatenate  the  2  LL's
+print('Linked  List  :  ' , end = '')
+a.disp()    # How  to  print  final  linked  list
+
+'''
+output:
+enter values terminated by ctrl+z  10 20 30 40 50
+enter values terminated by ctrl+z  60 70 80
+Linked  List  :  10 20  30  40  50  60  70  80  
+'''
+
+
+
+
+
+#  Write  a  method  to  copy  a  linked  list
+
+class  sll(linked_list):
+    def  copy(a):
+            b=sll() # How  to  create  a  local  object  for  2nd  linked  list
+            p=a.first
+            while p!=None:
+                new=node(p.data)
+                b.append(new)
+                p=p.link  # How  to  copy  each  node  of  1st  LL  to  2nd  LL  until   LL  is  exhausted
+            return b  # How  to   return  2nd  linked  list
+#  End  of  the  clas
+a=sll()
+a.create()  # How  to  create  1st  linked  list
+b=a.copy()  # How  to  copy  1st  linked  list  to  2nd  linked  list
+print('Original  linked   list  :  ' , end = '')
+a.disp()  # How  to  print  1st  linked  list
+print('Copied  linked   list  :  ' , end = '')
+b.disp()  # How  to  print  2nd  linked  list
+
+'''
+output:
+enter values terminated by ctrl+z
+Original  linked   list  :  10  20  40  50  
+Copied  linked   list  :  10    20  40  50  
+'''
+
+
+
+
+#  Write  destructor  to  delete  whole  linked  list
+
+class  sll(linked_list):
+    def __del__(a):
+        while not a.isempty():
+            temp=a.first
+            a.first=a.first.link
+            del temp # How  to  remove  each  node  of  LL  until  LL  is  empty
+        print('Linked  list  is  empty')
+#  End  of  the  clas
+a=sll()
+a.create()
+a.disp()
+del a # How  to  create  linked  list
+
+'''
+output:
+enter values terminated by ctrl+z
+10  20  30  40  50  
+Linked  list  is  empty
+'''
+
+
+
+
+'''
+Write  a  method  to  reverse  linked  list
+
+1) How  to  reverse  the  linked  list ?  --->  Modify  4th  node  link  to  3rd  node,
+                                                modify  3rd  node  link  to  2nd  node,
+                                                modify  2nd  node  link  to  1st  node,
+                                                modify  1st  node  link  to  NULL  and
+                                                modify  first  pointer  to  last  node
+
+2) How  many  references  are  needed  to  reverse  a  linked  list  ?  ---> Three  i.e.  prev , cur , next
+
+3) Where  does  ref  cur   points  to  (in  general) ?  ---> Current  node  i.e.  ith  node
+    Where  does  ref  prev   points  to ?  ---> Previous  node  i.e.  (i - 1)th  node
+    Where  does  ref  next  points  to ?  ---> Next  node  i.e.  (i + 1)th  node
+'''
+class  sll(linked_list):
+    def  reverse(a):
+        prev=None
+        cur=a.first
+        while cur!=None:
+            next=cur.link
+            cur.link=prev
+            prev=cur
+            cur=next  # How  to  reverse  each  node  of  linked  list
+        a.first=prev  # How  to  modify  ref  a . frist   to   last  node  of  linked  list
+# End  of  the  class
+a=sll()
+a.create()  # How  to  create  linked  list
+print('Input  Linked  List: ',a.disp())  # How  to  print  linked  list
+a.reverse()  # How  to  reverse  linked  list
+print('Reverse  Linked  List: ',a.disp())  # How  to  print  reverse  linked  list
+
+'''
+output:
+enter values terminated by ctrl+z
+Input  Linked  List: 10 20  30  40  50  
+Reverse  Linked  List: 50   40  30  20  10   
+'''
+
+# Find outputs  (Home  work)
+class  A:
+    def  m1(self):
+        super() . m1()
+        print('class A method')
+class  B:
+    def m1(self):
+        super() . m1()
+        print('class B method')
+class  C:
+    def m1(self):
+        super() . m1()
+        print('class C method')
+class  D:
+    def m1(self):
+        #super() . m1()
+        print('class D method')
+class  X(A , B):
+        def m1(self):
+                super() . m1()
+                print('class X method')
+class  Y(B , C , D):
+        def m1(self):
+                super() . m1()
+                print('class Y method')
+class  P(X , Y , C):
+        def m1(self):
+                super() . m1()
+                print('class P method')
+#end of the class
+print(P . mro()) # P -> X -> A -> Y -> B -> C -> D -> object
+obj = P()
+obj . m1()
+print('Bye')
+
+'''
+class D method
+class C method
+class B method
+class Y method
+class A method
+class X method
+class P method
+Bye
+'''
+# Find  outputs  (Home  work)
+class  D:
+        def __init__(self):
+                super() . __init__()
+                print('class D constructor')
+class  E:
+        def __init__(self):
+                super() . __init__()
+                print('class E constructor')
+class  F:
+        def __init__(self):
+                super() . __init__()
+                print('class F constructor')
+class  B(D , E):
+        def __init__(self):
+                super() . __init__()
+                print('class B constructor')
+class  C(D , E , F):
+        def __init__(self):
+                super() . __init__()
+                print('class C constructor')
+class  A(B , C):
+        def __init__(self):
+                super() . __init__()
+                print('class A constructor')
+#end of the class
+print(A . mro()) # A->B->C->D->E->F->object
+obj = A()
+print('Bye') #Bye
+'''
+class F constructor
+class E constructor
+class D constructor
+class C constructor
+class B constructor
+class A constructor
+'''
+
+# Identify  Error
+class  c1(c1):
+    pass 
+'''
+Self referential class  will raise a NameError 
+'''
+
+# Find  outputs
+class   c1:
+    def  m1(self):
+            print('Parent  Method')
+class  c1(c1):
+    def  m1(self):
+        super() . m1()
+        print('Child  Method')
+a = c1()
+a . m1()
+'''
+parent  Method
+Child  Method 
+'''
+# Identify  Error
+class   c1(c2):
+        pass
+class  c2(c1):
+    pass
+# c2 is used before declaration
+
+# Find  outputs
+class   c2:
+    def  m1(self):
+            print('Parent  Method')
+class   c1(c2):
+    def  m1(self):
+            super() . m1()
+            print('Child  Method')
+class  c2(c1):
+    def  m1(self):
+            super() . m1()
+            print('Grand  Child  Method')
+a = c2()
+a . m1()
+'''
+Parent  Method
+Child  Method
+Grand  Child  Method
+'''
