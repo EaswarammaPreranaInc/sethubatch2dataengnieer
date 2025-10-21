92a25372ba13754b5e05985f51298d205206a943
Sai <164913655+osc672@users.noreply.github.com>
2025-10-21 18:45:42 +0530

    Add files via upload

---
+++ saichand_SSSSDP-II-053/saichand_SSSSDP-II-053_(16-10-2025).py
@@ -0,0 +1,89 @@
+#Repeat  prog8b(fetchmany)  but  validate  input
+i.e. Print  a  msg  when  input > number  of  tuples
+
+Hint:  Use  fetchmany()  method
+'''
+Program:
+import mysql.connector
+try:    
+    con = mysql.connector.connect(
+        host='localhost',user='root',database='empdb'
+    )
+    table=input('Enter table name -')
+    num=int(input('Enter the number of rows to be displayed ?-'))
+    cur = con.cursor()
+    while table=='':
+        table=input('Enter table name -')
+    cur.execute(F'select Count(*) FROM {table}')
+    x=cur.fetchone()
+    lst=x[0]
+    if num<=lst:
+        cur.execute(F'select * FROM {table}')
+        list=cur.fetchmany(n)
+        for col in cur.description:
+            print(col[0], end='\t')
+        print()
+        for tpl in list:
+            for i in tpl:
+                    print(i, end='\t')
+            print()       
+    else:
+        print('Invalid input')
+    con.close()
+except AttributeError:
+    print('No of rows should not be negative')
+except mysql.connector.ProgrammingError as msg:
+    print(msg)
+
+
+
+
+
+
+
+
+'''
+Write  a  program  to  insert  multiple  rows  into  emp  table
+
+1) How  to  insert  multiple  rows  into  the  table ?  --->  With  executemany()  method
+
+2) Where  is  executemany()  method  defined ?  --->  In  MySqlCursor  class  (like  execute()  method)
+
+3) cur . executemany('insert   into  emp  values (%s,%s,%s)' ,  list)
+    What  does  the  method  do ?  ---> Inserts  all  the  tuples  of  the  list  into  emp  table
+
+4) What  is  first  %s  for ?  --->  First  element  of  each  tuple  in  the  list
+    What  is  2nd  %s  for ?  ---> 2nd  element  of  each  tuple  in  the  list
+    What  is  3rd  %s  for ?  ---> 3rd  element  of  each  tuple  in  the  list
+
+5) How  many  rows  are  inserted  if  there  are  four  tuples  in  the  list  ?  ---> 4  rows
+    What  is  the  result  of  cur . rowcount ? ---> 4
+
+6) What  are  the  two  arguments  of  executemany()  method  ?  --->
+sql  command   and   list  of  tuples  where  each  tuple  is  a  row
+'''
+Program:
+try:
+    import mysql.connector        
+    con = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            database='empdb'
+        )
+    cur = con.cursor()
+    n=int(input())
+    i=0
+    lst=[]
+    while i<n:
+            i+=1
+            print('Enter details for employee',i,'-')
+            empno = int(input('Enter empno: '))
+            ename = input('Enter ename: ')
+            sal = float(input('Enter salary: '))
+            data.append((empno,ename,sal))
+    cur.executemany('insert   into  emp  values (%s,%s,%s)' ,  lst)            
+    print(f'{cur.rowcount} rows inserted')
+    con.commit()
+    con.close()
+except mysql.connector.ProgrammingError as msg:
+    print("Error:", msg)
