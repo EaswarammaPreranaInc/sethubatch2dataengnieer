90c33b200b260d63d5b369b260c7e223a37f05ca
RAKSHITH-KOLIPAKA <rakshithkolipaka@gmail.com>
2025-10-29 16:26:43 +0530

    RAKSHITH_15-10-2025

---
+++ RAKSHITH_15-10.py
@@ -0,0 +1,58 @@
+
+
+Write  a  program  to  print  first  'n'  rows  of  emp  table
+
+ emp   table  --------------->   cur  object  --------------->    list   ------------->    tpl   ------------>   monitor
+                        execute()                           fetchmany(n)               for  loop                   print()
+
+
+Write  a  program  to  insert  rows  into  emp  table ,  one  at  a  time
+
+1) How  to  call  execute()  method ?  --->
+															cur . execute(F"insert  into  emp  values  ({empno} ,  '{ename}' , {sal})")
+
+2) Are  quotes  mandatory  for  ename ? --->  Yes  becoz  it  is  a  string
+
+3) What  is  the  pre-requisite  to  call  execute()  method ?  --->  Read  inputs  empno , ename  and  sal
+
+4) What  action  to  be  made  after  insert ?  --->  Call  commit()  method
+
+5) What  does  commit()  method  do ?  --->  Makes  insertion  becomes  permanent
+
+6) What  happens  when  commit()  is  not  called ?  --->  Insertion  is  only  temporary
+
+7) In  other  words,  insertion  does  not  happen
+
+8) Where  is  commit()  method  defined ?  ---> In  MySqlConnection  class
+
+9) cur . execute(F'insert  into  emp  values (25 , "Rama  Rao" , 10000.0)')
+    What  is  the  result  of  cur . rowcount ?  ---> 1  becoz  only  one  row  is  inserted  into  emp  table
+
+10) Can  a  tuple  be  inserted  into  cur  object ?  --->  No  becoz  it  is  immutable
+
+11) What  happens  when  we  try  to  insert  duplicate  empno ?  --->  Raises  mc . errors . IntegrityError
+
+
+Write  a   program  to  delete  rows  of  emp  table  based  on  user  input  condition
+
+1) How  to  call  execute()  method ?  --->   cur . execute(F'delete  from  emp  where  {cond}')
+
+2) What  is  the  pre-requisite  to  call  execute()  method ?  ---> Read  the  cond
+
+
+Write  a  program to  modify  data  of  emp  table
+
+1) How  to  call  execute()  method ?  --->  cur . execute(F'update  emp  set  {expr}   where  {cond}')
+
+2) What  is  the  pre-requisite  to  call  execute()  method ?  ---> Read  expr  and  cond
+
+
+Write  a  program  to  create  student  table
+
+1) How  to  call  execute()  method ?  --->
+									cur . execute(F'create  table  {tablename}(rollno  int  primary  key , sname  char(20) ,  marks  float)')
+
+2) What  is  the  pre-requisite  to  call  execute()  method ?  --->  Read  the  table  name
+
+3) What  action  to  be  made  when  table  already  exists ?  --->
+																	Delete  the  existing  table  and  create  a  new  table  with  same  name
