aecf07cd4503cc5d4f83544ec1192ae805ead5ed
cvviswanath <viswanathvenkataram@gmail.com>
2025-10-31 21:06:02 +0530

    viswanath_78_30.10.25.py

---
+++ viswanath_78_30.10.25.py
@@ -0,0 +1,67 @@
+Q)Modify  following  program  such  that  new  thread  should  wait  for  main  thread  expiry
+from threading import *
+def   disp():
+	for  i  in  range(10):
+		print('new  thread')
+new = Thread(target = disp)
+new . start()
+for  i  in  range(10):
+	print('main thread')
+Ans) from threading import *
+def disp():
+    main = main_thread()
+    main.join()
+    for i in range(10):
+        print('new thread')
+new = Thread(target=disp)
+new.start()
+for i in range(10):
+    print('main thread')
+
+# Find  outputs (Home work)
+from  threading  import  *
+main = main_thread()
+name  =  main.name
+print(name,' is started') #  MainThread is started
+main.join()
+print(name,'is ended') #  RuntimeError: cannot join current thread
+
+Q)Modify  following   program  such  that  t1  should  execute  double()  function  and t2  should  execute  square()  function
+from threading import *
+import time
+def   double():
+	for   i  in  range(1 , 7):
+		print('Double : ' , 2 * i)
+		time . sleep(1)
+def   square():
+	for   i  in   range(1 , 7):
+		print('Square : ' , i * i)
+		time . sleep(1)
+#  End  of  the  function
+start = time . time()
+double()
+square()
+end = time . time()
+print(end - start)  #  What  is   the   execution  time ?
+Ans) from threading import *
+import time
+def double():
+    for i in range(1, 7):
+        print('Double :', 2 * i)
+        time.sleep(1)
+def square():
+    for i in range(1, 7):
+        print('Square :', i * i)
+        time.sleep(1)
+# End of function
+t1 = Thread(target=double)
+t2 = Thread(target=square)
+start = time.time()
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+end = time.time()
+print(end - start)
+
+
