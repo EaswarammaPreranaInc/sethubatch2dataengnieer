49e75dbc49b4313c2eddce3e496c308bff4dbe8e
TALARI RAMANJANEYULU <talariramanjimb@gmail.com>
2025-10-31 23:27:04 +0530

    Create Enumerate_Class_Iterator_Examples_31_October_Dt.py

---
+++ Enumerate_Class_Iterator_Examples_31_October_Dt.py
@@ -0,0 +1,125 @@
+# Find  outputs (Home  work)
+import   time
+list = [25 , 10.8 , 'Hyd' , True]
+e = enumerate(list , start = 5)
+while   True:
+	try:
+		print(next(e))
+		time . sleep(1)
+	except  StopIteration:
+		break
+print(list[5]) # Raise IndexError 
+
+# Output :
+(5, 25)
+(6, 10.8)
+(7, 'Hyd')
+(8, True)
+
+
+'''  (Home  work)
+Can  string  be  enumerated ?
+'''
+import   time
+a = input('Enter  any  string  :  ') #  Assume  that  input  is  'Hyd'
+e = enumerate(a)
+while   True:
+	try:
+		print(next(e))
+		time . sleep(1)
+	except  StopIteration:
+		break
+
+# Output :
+Enter  any  string  :  Hyd
+(0, 'H')
+(1, 'y')
+(2, 'd')
+
+
+#  Can  set  be  enumerated  ?  (Home  work)
+import  time
+a = {25 , 10.8 , 'Hyd' , True}
+print(a)
+b = enumerate(a)
+while   True:
+	try:
+		print(next(b))
+		time . sleep(1)
+	except  StopIteration:
+		break
+
+# Output :
+{25, 10.8, 'Hyd', True}
+(0, 25)
+(1, 10.8)
+(2, 'Hyd')
+(3, True)
+
+
+# Can  dictionary  be  enumerated ?   (Home  work)
+import   time
+def  disp(e):
+	while  True:
+		try:
+			print(next(e))
+			time . sleep(1)
+		except:
+			break
+	print()
+a = {'Empno'  :  25 , 'Emp Name'  :  'Rama Rao' , 'Sal' : 10000.0}
+e1 = enumerate(a . keys())
+disp(e1)
+e2 = enumerate(a . values())
+disp(e2)
+e3 = enumerate(a . items())
+disp(e3)
+e4 = enumerate(a , start = 5)
+disp(e4)
+
+# Output :
+(0, 'Empno')
+(1, 'Emp Name')
+(2, 'Sal')
+
+(0, 25)
+(1, 'Rama Rao')
+(2, 10000.0)
+
+(0, ('Empno', 25))
+(1, ('Emp Name', 'Rama Rao'))
+(2, ('Sal', 10000.0))
+
+(5, 'Empno')
+(6, 'Emp Name')
+(7, 'Sal')
+
+
+
+# Find  outputs  (Home  work)
+import   time
+a = ['Telangana' , 'Andhra  Pradesh' , 'Karnataka' , 'TamilNadu' , 'Maharastra']
+b = ['Hyderabad' , 'Amaravathi' , 'Bangalore' , 'Chennai' , 'Mumbai']
+Write  code  to   print  the  following  outputs  using  enumerate  iterator
+Telangana        ... Hyderabad
+Andhra  Pradesh  ... Amaravathi
+Karnataka        ... Bangalore
+TamilNadu        ... Chennai
+Maharastra       ... Mumbai
+
+# Program 
+# Find  outputs  (Home  work)
+import   time
+a = ['Telangana' , 'Andhra  Pradesh' , 'Karnataka' , 'TamilNadu' , 'Maharastra']
+b = ['Hyderabad' , 'Amaravathi' , 'Bangalore' , 'Chennai' , 'Mumbai']
+e1=enumerate(a)
+for x,y in e1:
+    print(y,"...",b[x])
+    time.sleep(1)
+
+# Output :
+Telangana ... Hyderabad
+Andhra  Pradesh ... Amaravathi
+Karnataka ... Bangalore
+TamilNadu ... Chennai
+Maharastra ... Mumbai
