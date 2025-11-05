246c2adc4272a88132326add08f9c7a98ef06906
saicharan7702 <saicharanmadala19@gmail.com>
2025-11-05 22:47:30 +0530

    Add files via upload

---
+++ M.SAICHARAN_SSSDP_BATCH-II_70/M.SaiCharan (04-11-2025) SSSSDP-II 70.py
@@ -0,0 +1,45 @@
+                                    NAME:M.SAICHARAN                 HOMEWORK
+                                    DATE:04-11-2025
+
+1.# How  to  resolve  deadlock ?
+from  threading  import  *
+import  time
+def  f1():
+	l1 . acquire()
+	time . sleep(1)
+	l2 . acquire()
+	print('1st  thread  is  under  execution')
+	l2 . release()
+	l1 . release()
+	print('End  of  the  1st  thread')
+#  End  of  the  function
+def  f2():
+	l1 . acquire()
+	time . sleep(1)
+	l2 . acquire()
+	print('2nd   thread  is  under  execution')
+	l2 . release()
+	l1 . release()
+	print('End  of  the  2nd   thread')
+#  End  of  the  function
+l1 = Lock()
+l2 = Lock()
+t1 = Thread(target = f1)
+t2 = Thread(target = f2)
+t1 . start()
+t2 . start()
+t1 . join()
+t2 . join()
+print('End  of  main  thread')
+
+#Output:
+print("End of main thread")
+Expected output (No deadlock):
+text
+Thread 1 acquired l1
+1st thread is under execution
+End of the 1st thread
+Thread 2 acquired l1
+2nd thread is under execution
+End of the 2nd thread
+End of main thread
