c20bde647d56e7e2322978cb0e2769d76575d0be
vaishnavi-1102 <vadlakondavaishnavi2003@gmail.com>
2025-11-06 09:16:19 +0530

    Add files via upload

---
+++ Vaishnavi_Vadlakonda_SSSSDP-II-031/Vaishnavi Vadlakonda_SSSSDP-II-031_04-11-2025.py
@@ -0,0 +1,38 @@
+# How  to  resolve  deadlock ?
+from threading import *
+import time
+def f1():
+	l1 . acquire()
+	time . sleep(1)
+	l2 . acquire()
+	print('1st  thread  is  under  execution')
+	l2 . release()
+	l1 . release()
+	print('End  of  the  1st  thread')
+#  End  of  the  function
+def f2():
+	l1 . acquire()
+	time . sleep(1)
+	l2 . acquire()
+	print('2nd   thread  is  under  execution')
+	l2 . release()
+	l1 . release()
+	print('End  of  the  2nd   thread')
+#  End  of  the  function
+l1 = Lock()
+l2 = Lock()
+t1 = Thread(target = f1)
+t2 = Thread(target = f2)
+t1 . start()
+t2 . start()
+t1 . join()
+t2 . join()
+print('End  of  main thread')
+'''
+Outputs
+1st  thread  is  under  execution
+End  of  the  1st  thread
+2nd   thread  is  under  execution
+End  of  the  2nd   thread
+End  of  main thread
+'''
